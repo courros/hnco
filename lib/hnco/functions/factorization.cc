f00761c5a22f30df74b4ff7e45ba883b344264a3
Arnaud Berny <hnco@courros.fr>
2017-02-25 23:01:42 +0100

    Added factorization.hh and .cc
    Updated configure.ac to take into account library gmp.

---
+++ lib/hnco/functions/factorization.cc
@@ -0,0 +1,119 @@
+#include <assert.h>
+
+#include <iostream>
+
+#include "hnco/exception.hh"
+
+#include "factorization.hh"
+
+
+using namespace hnco::function;
+using namespace hnco::exception;
+
+
+Factorization::Factorization(std::string path)
+{
+  mpz_init(_number);
+  mpz_init(_first_factor);
+  mpz_init(_second_factor);
+  mpz_init(_product);
+
+  std::ifstream stream(path);
+  if (!stream.good()) {
+    std::ostringstream oss;
+    oss << "Factorization::Factorization: Cannot open " << path;
+    throw Error(oss.str());
+  }
+  std::string decimal;
+  stream >> decimal;
+
+  if (mpz_set_str(_number, decimal.c_str(), 10) != 0) {
+    std::ostringstream oss;
+    oss << "Factorization::Factorization: mpz_set_str failed";
+    throw Error(oss.str());
+  }
+
+  _number_size = mpz_sizeinbase(_number, 2);
+
+  if (_number_size % 2 == 0)
+    _first_factor_size = _number_size / 2;
+  else
+    _first_factor_size = _number_size / 2 + 1;
+
+  _second_factor_size = _number_size - 1;
+
+  _bv_size = _first_factor_size + _second_factor_size;
+
+  _first_factor_string = std::string(_first_factor_size, '0');
+  _second_factor_string = std::string(_second_factor_size, '0');
+}
+
+
+Factorization::~Factorization()
+{
+  mpz_clear(_number);
+  mpz_clear(_first_factor);
+  mpz_clear(_second_factor);
+  mpz_clear(_product);
+}
+
+
+void
+Factorization::convert(const bit_vector_t& x)
+{
+  assert(x.size() == _bv_size);
+  assert(x.size() == _first_factor_size + _second_factor_size);
+
+  for (size_t i = 0; i < _first_factor_size; i++)
+    if (x[i])
+      _first_factor_string[i] = '1';
+    else
+      _first_factor_string[i] = '0';
+  mpz_set_str(_first_factor, _first_factor_string.c_str(), 2);
+
+  for (size_t i = 0; i < _second_factor_size; i++)
+    if (x[_first_factor_size + i])
+      _second_factor_string[i] = '1';
+    else
+      _second_factor_string[i] = '0';
+  mpz_set_str(_second_factor, _second_factor_string.c_str(), 2);
+
+}
+
+
+double
+Factorization::eval(const bit_vector_t& x)
+{
+  convert(x);
+  mpz_mul(_product, _first_factor, _second_factor);
+
+  assert(mpz_sgn(_first_factor) >= 0);
+  assert(mpz_sgn(_second_factor) >= 0);
+
+  return -double(mpz_hamdist(_product, _number));
+}
+
+
+void
+Factorization::display(std::ostream& stream)
+{
+}
+
+
+void
+Factorization::display(const bit_vector_t& x, std::ostream& stream)
+{
+  convert(x);
+  mpz_mul(_product, _first_factor, _second_factor);
+
+  std::cout << "Bit vector:" << std::endl;
+  bv_display(x, std::cout);
+  std::cout << std::endl << std::endl;
+
+  std::cout << "Interpreted as:" << std::endl
+            << _first_factor_string << " x " << _second_factor_string << std::endl << std::endl;
+
+  std::cout << "In decimal form:" << std::endl;
+  gmp_printf("%Zd x %Zd = %Zd (%Zd)\n", _first_factor, _second_factor, _product, _number);
+
+}
---
+++ lib/hnco/functions/factorization.hh
@@ -0,0 +1,92 @@
+#ifndef HNCO_FUNCTIONS_FACTORIZATION_H
+#define HNCO_FUNCTIONS_FACTORIZATION_H
+
+#include <gmp.h>
+
+#include <vector>
+#include <string>
+
+#include "function.hh"
+
+
+namespace hnco {
+namespace function {
+
+
+  /// Factorization
+  class Factorization:
+    public Function {
+
+    /// Number to factorize
+    mpz_t _number;
+
+    /// First factor
+    mpz_t _first_factor;
+
+    /// Second factor
+    mpz_t _second_factor;
+
+    /// Product
+    mpz_t _product;
+
+    /// First factor in binary form
+    std::string _first_factor_string;
+
+    /// Secon factor in binary form
+    std::string _second_factor_string;
+
+    /// Number size in bits
+    size_t _number_size;
+
+    /// First factor size in bits
+    size_t _first_factor_size;
+
+    /// Second factor size in bits
+    size_t _second_factor_size;
+
+    /// Bit vector size
+    size_t _bv_size;
+
+    /// Convert a bit vector into two numbers
+    void convert(const bit_vector_t& x);
+
+  public:
+
+    /** Constructor.
+
+        \param path Path to a file containing a number to factorize
+
+        \warning The file is a text file which contains exactly one
+        natural number written in base 10 without any space.
+     */
+    Factorization(std::string path);
+
+    /// Destructor
+    ~Factorization();
+
+    /// Get bit vector size
+    size_t get_bv_size() { return _bv_size; }
+
+    /// Evaluate a bit vector
+    double eval(const bit_vector_t&);
+
+    /// Check for a known maximum.
+    bool has_known_maximum() { return true; }
+
+    /// Get the global maximum
+    double get_maximum() { return 0; }
+
+    /// Display
+    void display(std::ostream& stream);
+
+    /// Display
+    void display(const bit_vector_t& x, std::ostream& stream);
+
+  };
+
+
+} // end of namespace function
+} // end of namespace hnco
+
+
+#endif
