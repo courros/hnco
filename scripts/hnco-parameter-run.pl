08ee28b6550c72681f996b0ef9e9d4bd9d78a804
Arnaud Berny <hnco@courros.fr>
2017-10-01 14:21:12 +0200

    Use functions to iterate over values

--- scripts/hnco-parameter-run.pl
+++ scripts/hnco-parameter-run.pl
@@ -23,90 +23,84 @@ my $plan = "plan.json";
 if (@ARGV) {
     $plan = shift @ARGV;
 }
-
 print "Using $plan\n";
 
 open(FILE, $plan) || die "hnco-parameter-run: Cannot open $plan\n";
-
 my $json = "";
 while (<FILE>) {
     $json .= $_;
 }
-
 my $obj = from_json($json);
 
+my $exec                = $obj->{exec};
 my $path_results        = $obj->{results};
 my $num_runs            = $obj->{num_runs};
 my $functions           = $obj->{functions};
 my $algorithms          = $obj->{algorithms};
-
 my $parameter           = $obj->{parameter};
 my $parameter_id        = $parameter->{id};
-my $values              = $parameter->{values};
+
+my $values;
+if ($parameter->{values_perl}) {
+    my @tmp = eval $parameter->{values_perl};
+    $values = \@tmp;
+} else {
+    $values = $parameter->{values};
+}
 
 unless (-d $path_results) {
     mkdir $path_results;
     print "Created $path_results\n";
 }
 
-foreach my $f (@$functions) {
-    my $function_id = $f->{id};
-    print "$function_id\n";
-    unless (-d "$path_results/$function_id") {
-        mkdir "$path_results/$function_id";
-        print "Created $path_results/$function_id\n";
-    }
+iterate_functions();
 
+sub iterate_functions
+{
+    foreach my $f (@$functions) {
+        my $function_id = $f->{id};
+        print "$function_id\n";
+        my $path = "$path_results/$function_id";
+        unless (-d $path) {
+            mkdir $path;
+            print "Created $path\n";
+        }
+        iterate_algorithms($path, "$obj->{opt} $f->{opt}");
+    }
+}
+
+sub iterate_algorithms
+{
+    my ($prefix, $opt) = @_;
     foreach my $a (@$algorithms) {
         my $algorithm_id = $a->{id};
         print "$algorithm_id\n";
-        unless (-d "$path_results/$function_id/$algorithm_id") {
-            mkdir "$path_results/$function_id/$algorithm_id";
-            print "Created $path_results/$function_id/$algorithm_id\n";
+        my $path = "$prefix/$algorithm_id";
+        unless (-d $path) {
+            mkdir "$path";
+            print "Created $path\n";
         }
-        my $exec = $obj->{exec};
-        if (exists($a->{exec})) {
-            $exec = $a->{exec};
-        }
-
-        foreach my $value (@$values) {
-            print "With --$parameter_id $value: ";
-
-            my $output = "$path_results/$function_id/$algorithm_id/$parameter_id-$value.dat";
-
-            my $opt = "$a->{opt}";
-            if (exists($a->{dependant})) {
-                my $dependant = eval $a->{dependant};
-                print "With $dependant: ";
-                $opt .= " $dependant";
-            }
-
-            my $full_command = join ' ', ("$exec",
-                                          "$obj->{opt}",
-                                          "$f->{opt}",
-                                          "$opt",
-                                          "--$parameter_id $value",
-                                          ">> $output 2>> log.err");
-
-            system("rm -rf $output");
-            if ($a->{deterministic}) {
-                system("$full_command");
-                print ".";
-            } else {
-                foreach (1..$num_runs) {
-                    system("$full_command");
-                    print ".";
-                }
-            }
-
-            print "\n";
-
-        }
-
-        print "\n";
-
+        iterate_values($path, "$opt $a->{opt}", $a);
     }
+}
 
-    print "\n";
-
+sub iterate_values
+{
+    my ($prefix, $opt, $a) = @_;
+    foreach my $value (@$values) {
+        print "$parameter_id = $value: ";
+        my $output = "$prefix/$parameter_id-$value.dat";
+        system("rm -rf $output");
+        my $command = "$exec $opt --$parameter_id $value >> $output 2>> log.err";
+        if ($a->{deterministic}) {
+            system("$command");
+            print ".";
+        } else {
+            foreach (1..$num_runs) {
+                system("$command");
+                print ".";
+            }
+        }
+        print "\n";
+    }
 }
