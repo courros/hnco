a07488deffef7afb13788f314fb7f0dd4bbdb98f
Arnaud Berny <hnco@courros.fr>
2017-11-10 19:52:35 +0100

    Add BernoulliProcessMutation

--- lib/hnco/neighborhoods/mutation.hh
+++ lib/hnco/neighborhoods/mutation.hh
@@ -36,26 +36,38 @@ namespace neighborhood {
   /// Mutation
   class Mutation {
 
-  protected:
-
   public:
 
     /// Destructor
     virtual ~Mutation() {}
 
-    /// Mutate
-    virtual void mutate(bit_vector_t& input) = 0;
+    /** Mutate.
 
-    /// Mutate
-    virtual void mutate2(const bit_vector_t& input, bit_vector_t& output) = 0;
+        In-place mutation of the bit vector.
+
+        \param bv Bit vector to mutate
+    */
+    virtual void mutate(bit_vector_t& bv) = 0;
+
+    /** Map.
+
+        The output bit vector is a mutated version of the input bit
+        vector.
+
+        \param input Input bit vector
+        \param output Output bit vector
+    */
+    virtual void map(const bit_vector_t& input, bit_vector_t& output) = 0;
 
   };
 
 
   /// Single bit flip mutation
-  class SingleBitFlipMutation {
+  class SingleBitFlipMutation:
+    public Mutation {
 
-  protected:
+    /// Bit vector size
+    size_t _bv_size;
 
     /// Uniform index distribution
     std::uniform_int_distribution<int> _uniform_index_dist;
@@ -67,18 +79,65 @@ namespace neighborhood {
         \param n Size of bit vectors
     */
     SingleBitFlipMutation(int n):
+      _bv_size(n),
       _uniform_index_dist(0, n - 1) {}
 
-    /// Mutate
-    void mutate(bit_vector_t& input) {
-      bv_flip(input, _uniform_index_dist(random::Random::engine));
+    void mutate(bit_vector_t& bv) {
+      assert(bv.size() == _bv_size);
+      bv_flip(bv, _uniform_index_dist(random::Random::engine));
     }
 
-    /// Mutate
-    void mutate2(const bit_vector_t& input, bit_vector_t& output) {
+    void map(const bit_vector_t& input, bit_vector_t& output) {
+      assert(input.size() == _bv_size);
       assert(input.size() == output.size());
       copy(input.begin(), input.end(), output.begin());
       bv_flip(output, _uniform_index_dist(random::Random::engine));
+    }
+
+  };
+
+
+  /** Bernoulli process mutation.
+
+      Each component of the bit vector is flipped with some fixed
+      probability. If no component has been flipped at the end, the
+      process is started all over again. Thus the number of flipped
+      bits follows a pseudo binomial law.
+
+  */
+  class BernoulliProcessMutation:
+    public Mutation {
+
+    /// Bit vector size
+    size_t _bv_size;
+
+    /// Bernoulli distribution (biased coin)
+    std::bernoulli_distribution _bernoulli_dist;
+
+    /// Binomial distribution
+    std::binomial_distribution<int> _binomial_dist;
+
+    /// Reservoir sampling
+    bool _reservoir_sampling = false;
+
+  public:
+
+    /** Constructor.
+
+        \param n Size of bit vectors
+    */
+    BernoulliProcessMutation(int n):
+      _bv_size(n) {}
+
+    void mutate(bit_vector_t& bv) {
+      assert(bv.size() == _bv_size);
+
+    }
+
+    void map(const bit_vector_t& input, bit_vector_t& output) {
+      assert(input.size() == _bv_size);
+      assert(input.size() == output.size());
+
     }
 
   };
