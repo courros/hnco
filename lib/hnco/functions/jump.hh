e58f0cd03c34ef9cf2aa66bf6ed364146690cbf1
Arnaud Berny <hnco@courros.fr>
2017-11-28 17:33:25 +0100

    Document get_maximum

--- lib/hnco/functions/jump.hh
+++ lib/hnco/functions/jump.hh
@@ -89,7 +89,8 @@ namespace hnco::function {
         \return true */
     bool has_known_maximum() { return true; }
 
-    /// Get the global maximum
+    /** Get the global maximum.
+        \return _bv_size + _gap */
     double get_maximum() { return _bv_size + _gap; }
 
   };
