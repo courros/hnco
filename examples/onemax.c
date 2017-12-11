8d18ca19e60293275d140cba5070d67a2520e474
Arnaud Berny <hnco@courros.fr>
2017-12-11 19:57:00 +0100

    Add license statement

--- examples/onemax.c
+++ examples/onemax.c
@@ -1,3 +1,23 @@
+/* Copyright (C) 2016, 2017 Arnaud Berny
+
+   This file is part of HNCO.
+
+   HNCO is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   HNCO is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+   Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with HNCO. If not, see
+   <http://www.gnu.org/licenses/>.
+
+*/
+
 #include <stdlib.h>             /* size_t */
 
 double onemax(const char *data, size_t len)
