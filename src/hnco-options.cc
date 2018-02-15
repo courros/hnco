030142496dc92640c446f35bdde3bc06ef4d25c7
Arnaud Berny <hnco@courros.fr>
2018-02-15 14:02:50 +0100

    Update version and copyright years

--- src/hnco-options.cc
+++ src/hnco-options.cc
@@ -8,7 +8,7 @@ using namespace std;
 
 Options::Options(int argc, char *argv[]):
   _exec_name(argv[0]),
-  _version("0.7"),
+  _version("0.8"),
   _algorithm(100),
   _opt_algorithm(false),
   _bm_mc_reset_strategy(1),
