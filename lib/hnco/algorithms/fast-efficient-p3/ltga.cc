d2accabe26570aa76f49a8ccb783bec3166fa319
Arnaud Berny <hnco@courros.fr>
2018-07-18 14:58:26 +0200

    Add files for ltga

---
+++ lib/hnco/algorithms/fast-efficient-p3/ltga.cc
@@ -0,0 +1,59 @@
+/* Copyright (C) 2016, 2017, 2018 Arnaud Berny
+
+   This file is part of HNCO.
+
+   HNCO is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   HNCO is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+   Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with HNCO. If not, see
+   <http://www.gnu.org/licenses/>.
+
+*/
+
+#include <memory>               // std::shared_ptr
+
+#include "hnco/random.hh"
+
+#include "hboa.hh"
+#include "hnco-evaluator.hh"
+
+#include "src/Configuration.h"
+#include "src/MiddleLayer.h"
+#include "src/HBOA.h"
+
+using namespace hnco::algorithm::fast_efficient_p3;
+using namespace hnco::algorithm;
+using namespace hnco;
+
+
+void
+Hboa::maximize()
+{
+  Configuration configuration;
+
+  configuration.set("verbosity", 0);
+  configuration.set("solution_file", std::string("hboa-solution.txt"));
+  configuration.set("disable_solution_outfile", 1);
+
+  configuration.set("length", _solution.first.size());
+  configuration.set("pop_size", _population_size);
+  configuration.set("hill_climber", std::string("no_action"));
+
+  std::shared_ptr<HncoEvaluator> evaluator(new HncoEvaluator(_function));
+  std::shared_ptr<Middle_Layer> middle_layer(new Middle_Layer(configuration, evaluator));
+
+  HBOA hboa(hnco::random::Random::engine, middle_layer, configuration);
+
+  while (hboa.iterate()) {}
+
+  bv_from_vector_bool(_solution.first, middle_layer->best_solution);
+  _solution.second = middle_layer->best_fitness;
+}
---
+++ lib/hnco/algorithms/fast-efficient-p3/ltga.hh
@@ -0,0 +1,67 @@
+/* Copyright (C) 2016, 2017, 2018 Arnaud Berny
+
+   This file is part of HNCO.
+
+   HNCO is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   HNCO is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+   Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with HNCO. If not, see
+   <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef HNCO_ALGORITHMS_FAST_EFFICIENT_P3_BOA_H
+#define HNCO_ALGORITHMS_FAST_EFFICIENT_P3_BOA_H
+
+#include "hnco/algorithms/algorithm.hh"
+#include "hnco/exception.hh"
+
+
+/// Fast efficient P3
+namespace hnco::algorithm::fast_efficient_p3 {
+
+  /** Hboa.
+
+      Implementation of the Hierarchical Bayesian Optimization
+      Algorithm and helper classes based on the publication: Pelikan,
+      M. and Goldberg, D. (2006). Hierarchical bayesian optimization
+      algorithm. In Scalable Optimization via Probabilistic Modeling,
+      volume 33 of Studies in Computational Intelligence, pages
+      63–90. Springer Berlin Heidelberg.
+
+      Author: Brian Goldman
+
+      Integrated into HNCO by Arnaud Berny
+  */
+  class Hboa:
+    public Algorithm {
+
+    /// Population size
+    int _population_size = 10;
+
+  public:
+
+    /// Constructor
+    Hboa(int n):
+      Algorithm(n) {}
+
+    /// Maximize
+    void maximize();
+
+    /// Set population size
+    void set_population_size(int n) { _population_size = n; }
+
+  };
+
+} // end of namespace hnco::algorithm::fast_efficient_p3
+
+
+#endif
