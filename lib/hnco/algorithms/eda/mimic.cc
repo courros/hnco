b5d40c2b8cf3f105517751cb9319f1c5074493f3
Arnaud Berny <hnco@courros.fr>
2018-07-24 22:25:14 +0200

    Add mimic files

---
+++ lib/hnco/algorithms/eda/mimic.cc
@@ -0,0 +1,59 @@
+/* Copyright (C) 2016, 2017, 2018 Arnaud Berny
+
+   This file is part of HNCO.
+
+   HNCO is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   HNCO is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+   Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with HNCO. If not, see
+   <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "umda.hh"
+
+
+using namespace hnco::function;
+using namespace hnco::algorithm;
+using namespace hnco;
+
+
+void
+Umda::init()
+{
+  random_solution();
+  pv_uniform(_pv);
+}
+
+
+void
+Umda::iterate()
+{
+  for (size_t i = 0; i < _population.size(); i++)
+    pv_sample(_pv, _population.get_bv(i));
+
+  if (_functions.size() > 1)
+    _population.eval(_functions);
+  else
+    _population.eval(_function);
+
+  _population.sort();
+
+  update_solution(_population.get_best_bv(),
+                  _population.get_best_value());
+
+  pv_init(_pv);
+  for (int i = 0; i < _selection_size; i++)
+    pv_add(_pv, _population.get_best_bv(i));
+  pv_average(_pv, _selection_size);
+  pv_bound(_pv, _lower_bound, _upper_bound);
+
+}
---
+++ lib/hnco/algorithms/eda/mimic.hh
@@ -0,0 +1,88 @@
+/* Copyright (C) 2016, 2017, 2018 Arnaud Berny
+
+   This file is part of HNCO.
+
+   HNCO is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   HNCO is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+   Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with HNCO. If not, see
+   <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef HNCO_ALGORITHMS_PV_UMDA_H
+#define HNCO_ALGORITHMS_PV_UMDA_H
+
+#include "hnco/algorithms/population.hh"
+
+#include "pv-algorithm.hh"
+
+
+namespace hnco::algorithm {
+
+  /** Univariate marginal distribution algorithm.
+
+      Reference:
+
+      H. Mühlenbein. 1997. The equation for response to selection and
+      its use for prediction. Evolutionary Computation 5, 3 (1997),
+      303–346.
+
+  */
+  class Umda:
+    public PvAlgorithm {
+
+  protected:
+
+    /// Population
+    Population _population;
+
+    /// Mean of selected bit vectors
+    pv_t _mean;
+
+    /** @name Parameters
+     */
+    ///@{
+
+    /// Selection size
+    int _selection_size = 1;
+
+    ///@}
+
+    /// Single iteration
+    void iterate();
+
+  public:
+
+    /// Constructor
+    Umda(int n, int population_size):
+      PvAlgorithm(n),
+      _population(population_size, n),
+      _mean(n) {}
+
+    /// Initialization
+    void init();
+
+    /** @name Setters
+     */
+    ///@{
+
+    /// Set the selection size
+    void set_selection_size(int x) { _selection_size = x; }
+
+    ///@}
+
+  };
+
+} // end of namespace hnco::algorithm
+
+
+#endif
