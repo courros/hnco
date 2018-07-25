23e3a197c04871077dc20bc1384f37afc933c762
Arnaud Berny <hnco@courros.fr>
2018-07-25 19:21:33 +0200

    Implement model update

--- lib/hnco/algorithms/eda/mimic.cc
+++ lib/hnco/algorithms/eda/mimic.cc
@@ -18,8 +18,11 @@
 
 */
 
-#include <algorithm>            // std::fill
+#include <algorithm>            // std::fill, std::min, std::swap, std::min_element
 
+#include <math.h>               // std::log
+
+#include "hnco/algorithms/pv/probability-vector.hh"
 #include "hnco/random.hh"
 
 #include "mimic.hh"
@@ -73,5 +76,76 @@ Mimic::iterate()
   update_solution(_population.get_best_bv(),
                   _population.get_best_value());
 
-  // update model
+  pv_init(_mean);
+  for (int i = 0; i < _selection_size; i++)
+    pv_add(_mean, _population.get_best_bv(i));
+  pv_average(_mean, _selection_size);
+
+  /* Choose component (root) with the smallest entropy or with
+     probability closest to 0 or 1 */
+
+  std::transform(_mean.begin(), _mean.end(), _entropies.begin(), [](double x){ return std::min(x, 1 - x); });
+  std::size_t root = std::min_element(_entropies.begin(), _entropies.end()) - _entropies.begin();
+  assert(root < _permutation.size());
+
+  perm_identity(_permutation);
+  _permutation[0] = root;
+  _permutation[root] = 0;
+  _parameters[0].first = _mean[root];
+  _parameters[0].second = _mean[root];
+
+  for (std::size_t i = 1; i < _permutation.size(); i++) {
+
+    std::size_t first = _permutation[i - 1];
+
+    for (std::size_t j = i; j < _permutation.size(); j++) {
+
+      std::size_t second = _permutation[j];
+      assert(j != i);
+
+      _table.fill({0, 0});
+
+      for (int k = 0; k < _selection_size; k++) {
+        const bit_vector_t& bv = _population.get_best_bv(k);
+        _table[bv[first]][bv[second]]++;
+      }
+
+      _entropies[j] = 0;
+
+      int c0 = _table[0][0] + _table[0][1];
+      if (c0) {
+        if (_table[0][0])
+          _entropies[j] += _table[0][0] * std::log(double(c0) / _table[0][0]);
+        if (_table[0][1])
+          _entropies[j] += _table[0][1] * std::log(double(c0) / _table[0][1]);
+      }
+
+      int c1 = _table[1][0] + _table[1][1];
+      if (c1) {
+        if (_table[1][0])
+          _entropies[j] += _table[1][0] * std::log(double(c1) / _table[1][0]);
+        if (_table[1][1])
+          _entropies[j] += _table[1][1] * std::log(double(c1) / _table[1][1]);
+      }
+
+      assert(c0 + c1 == _selection_size);
+      assert(_entropies[j] >= 0);
+
+      _parameters[i].first = c1 ? _table[1][1] / double(c1) : 0.5;
+      _parameters[i].second = c0 ? _table[0][1] / double(c0) : 0.5;
+
+    }
+
+    std::size_t child = std::min_element(_entropies.begin() + i, _entropies.end()) - _entropies.begin();
+    assert(child < _permutation.size());
+
+    if (child != i) {
+      std::swap(_permutation[i], _permutation[child]);
+      std::swap(_parameters[i], _parameters[child]);
+    }
+
+  }
+
+  assert(perm_is_valid(_permutation));
+
 }
