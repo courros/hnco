07ea4631931b43c3cf633ee600d49d9309eed7d3
Arnaud Berny <hnco@courros.fr>
2018-10-20 12:44:37 +0200

    Document member functions

--- lib/hnco/functions/long-path.hh
+++ lib/hnco/functions/long-path.hh
@@ -31,8 +31,8 @@ namespace function {
   /** Long path.
 
       Long paths have been introduced by Jeffrey Horn, David
-      E. Goldberg, and Kalyanmoy Deb. Here we follow the definition
-      given by Thomas Jansen (see references below).
+      E. Goldberg, and Kalyanmoy Deb. Here we mostly follow the
+      definition given by Thomas Jansen (see references below).
 
       As an example, here is the 2-long path of dimension 4:
       - 0000
@@ -46,6 +46,10 @@ namespace function {
       The fitness is increasing along the path. The fitness on the
       complementary of the path is defined as a linear function
       pointing to the beginning of the path.
+
+      To help with the detection of maximum, we have dropped the
+      constant \f$n^2\f$ whose sole purpose was to make the function
+      non negative.
 
       References:
 
@@ -79,10 +83,28 @@ namespace function {
     /// Get bit vector size
     size_t get_bv_size() { return _bv_size; }
 
-    /// Check for a known maximum.
+    /** Check for a known maximum.
+
+        Let \f$n\f$ be the bit vector size and \f$k\f$ the prefix
+        length which must divide \f$n\f$.
+
+        We have to check that the maximum can be represented exactly
+        as a double, that is, it must be lower or equal to
+        \f$2^{53}\f$. We are a little bit more conservative with the
+        following test.
+
+        If \f$\log_2(k) + n / k \le 53\f$ then returns true else
+        returns false.
+
+    */
     bool has_known_maximum();
 
     /** Get the global maximum.
+
+        Let \f$n\f$ be the bit vector size and \f$k\f$ the prefix
+        length which must divide \f$n\f$. Then the maximum is \f$k
+        2^{n/k} - k + 1\f$.
+
         \throw Error */
     double get_maximum();
 
