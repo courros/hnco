1f31cd5abe64cea8387ff4aa68f7e55c518557a2
Arnaud Berny <hnco@courros.fr>
2018-10-23 17:07:30 +0200

    Define FunctionDecorator::display and describe

--- lib/hnco/functions/decorators/function-decorator.hh
+++ lib/hnco/functions/decorators/function-decorator.hh
@@ -48,6 +48,19 @@ namespace hnco::function {
     FunctionDecorator(Function *function):
       _function(function) {}
 
+
+    /** @name Display
+     */
+    ///@{
+
+    /// Display
+    void display(std::ostream& stream) { _function->display(stream); }
+
+    /// Describe a bit vector
+    void describe(const bit_vector_t& x, std::ostream& stream) { _function->describe(x, stream); }
+
+    ///@}
+
   };
 
 } // end of namespace hnco::function
