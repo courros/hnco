984f835d2101ba1380619aeaa126456aa24822de
Arnaud Berny <hnco@courros.fr>
2018-11-11 16:38:44 +0100

    Add test-injection-projection.cc

---
+++ tests/test-injection-projection.cc
@@ -0,0 +1,49 @@
+/* Copyright (C) 2016, 2017, 2018 Arnaud Berny
+
+   This file is part of HNCO.
+
+   HNCO is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   HNCO is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+   Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with HNCO. If not, see
+   <http://www.gnu.org/licenses/>.
+
+*/
+
+#include <chrono>
+#include <random>
+
+#include "hnco/bit-vector.hh"
+#include "hnco/random.hh"
+
+using namespace hnco::random;
+using namespace hnco;
+
+
+int main(int argc, char *argv[])
+{
+  Random::engine.seed(std::chrono::system_clock::now().time_since_epoch().count());
+
+  std::uniform_int_distribution<int> bv_size_dist(0, 64);
+
+  for (int i = 0; i < 1000; i++) {
+    int bv_size = bv_size_dist(Random::engine);
+    bit_vector_t src(bv_size);
+    bv_random(src);
+    std::size_t index = bv_to_size_type(src);
+    bit_vector_t dest(bv_size);
+    bv_from_size_type(dest, index);
+    if (dest != src)
+      return 1;
+  }
+
+  return 0;
+}
