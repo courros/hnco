ad39cf1052350637a3ae1dfdd9496580c5a17108
Arnaud Berny <hnco@courros.fr>
2019-04-18 00:24:16 +0200

    Add files for nearest-neighbor-ising-model-2

---
+++ lib/hnco/functions/ising/nearest-neighbor-ising-model-2.cc
@@ -0,0 +1,166 @@
+/* Copyright (C) 2016, 2017, 2018, 2019 Arnaud Berny
+
+   This file is part of HNCO.
+
+   HNCO is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   HNCO is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+   Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with HNCO. If not, see
+   <http://www.gnu.org/licenses/>.
+
+*/
+
+#include <assert.h>
+
+#include "hnco/random.hh"
+
+#include "nearest-neighbor-ising-model-1.hh"
+
+
+using namespace hnco::random;
+using namespace hnco::function;
+
+
+void
+NearestNeighborIsingModel1::resize(int n)
+{
+  assert(n > 0);
+
+  _couplings.resize(n);
+  _field.resize(n);
+  _flipped_bits = bit_vector_t(n, 0);
+}
+
+
+void
+NearestNeighborIsingModel1::random(int n)
+{
+  assert(n > 0);
+
+  resize(n);
+  for (size_t i = 0; i < _couplings.size(); i++) {
+    _couplings[i] = Random::normal();
+    _field[i] = Random::normal();
+  }
+}
+
+
+double
+NearestNeighborIsingModel1::eval(const bit_vector_t& s)
+{
+  assert(_couplings.size() > 0);
+  assert(s.size() == _couplings.size());
+
+  const size_t n = _couplings.size();
+  const size_t last = n - 1;
+
+  double result = 0;
+
+  // Interactions
+  for (size_t i = 0; i < last; i++)
+    if ((s[i] + s[i + 1]) % 2 == 0)
+      result += _couplings[i];
+    else
+      result -= _couplings[i];
+  if (_periodic_boundary_conditions) {
+    if ((s[last] + s[0]) % 2 == 0)
+      result += _couplings[last];
+    else
+      result -= _couplings[last];
+  }
+
+  // External field
+  for (size_t i = 0; i < n; i++)
+    if (s[i])
+      result -= _field[i];
+    else
+      result += _field[i];
+
+  return result;
+}
+
+
+double
+NearestNeighborIsingModel1::incremental_eval(const bit_vector_t& x,
+                                             double value,
+                                             const hnco::sparse_bit_vector_t& flipped_bits)
+{
+  assert(_couplings.size() > 0);
+  assert(x.size() == _couplings.size());
+
+  const size_t n = _couplings.size();
+  const size_t last = n - 1;
+
+  assert(bv_is_zero(_flipped_bits));
+  bv_flip(_flipped_bits, flipped_bits);
+
+  // Interactions with the right site
+  for (auto index : flipped_bits) {
+    size_t next;
+    if (index == last) {
+      if (_periodic_boundary_conditions)
+        next = 0;
+      else
+        continue;
+    } else {
+      next = index + 1;
+      assert(next < n);
+    }
+    if (_flipped_bits[next])
+      continue;
+    if ((x[index] + x[next]) % 2 == 0)
+      value -= 2 * _couplings[index];
+    else
+      value += 2 * _couplings[index];
+  }
+
+  // Interactions with the left site
+  for (auto index : flipped_bits) {
+    size_t previous;
+    if (index == 0) {
+      if (_periodic_boundary_conditions)
+        previous = last;
+      else
+        continue;
+    } else {
+      previous = index - 1;
+      assert(previous < n);
+    }
+    if (_flipped_bits[previous])
+      continue;
+    if ((x[previous] + x[index]) % 2 == 0)
+      value -= 2 * _couplings[previous];
+    else
+      value += 2 * _couplings[previous];
+  }
+
+  bv_flip(_flipped_bits, flipped_bits);
+  assert(bv_is_zero(_flipped_bits));
+
+  // External field
+  for (auto index : flipped_bits) {
+    if (x[index])
+      value += 2 * _field[index];
+    else
+      value -= 2 * _field[index];
+  }
+
+  return value;
+}
+
+
+void
+NearestNeighborIsingModel1::display(std::ostream& stream)
+{
+  stream << "NearestNeighborIsingModel1" << std::endl;
+  stream << "n = " << get_bv_size() << std::endl;
+  stream << "Periodic boundary condition = " << _periodic_boundary_conditions << std::endl;
+}
---
+++ lib/hnco/functions/ising/nearest-neighbor-ising-model-2.hh
@@ -0,0 +1,168 @@
+/* Copyright (C) 2016, 2017, 2018, 2019 Arnaud Berny
+
+   This file is part of HNCO.
+
+   HNCO is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   HNCO is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+   Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with HNCO. If not, see
+   <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef HNCO_FUNCTIONS_ISING_NEAREST_NEIGHBOR_ISING_MODEL_1_H
+#define HNCO_FUNCTIONS_ISING_NEAREST_NEIGHBOR_ISING_MODEL_1_H
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/serialization/vector.hpp>
+
+#include "hnco/functions/function.hh"
+
+
+namespace hnco {
+namespace function {
+
+
+  /** Nearest neighbor Ising model in one dimension.
+
+      Its expression is of the form
+
+      \f$ f(x) = \sum_i J_{i,i+1} (1 - 2x_i)(1 - 2x_{i+1}) + \sum_i h_i (1 - 2x_i)\f$
+
+      or equivalently
+
+      \f$ f(x) = \sum_i J_{i,i+1} (-1)^{x_i + x_{i+1}} + \sum_i h_i
+      (-1)^{x_i}\f$
+
+      where \f$J_{i,i+1}\f$ is the interaction between adjacent sites
+      i and i+1 and \f$h_i\f$ is the external magnetic field
+      interacting with site i.
+
+      In the case of perdiodic boundary conditions, the sum \f$i+1\f$
+      is mod n.
+
+      Since we are maximizing f or minimizing -f, the expression of f
+      is compatible with what can be found in physics textbooks.
+
+      It should be noted that such an Ising model can be represented
+      by a Walsh expansion of degree 2, that is WalshExpansion2.
+
+      Reference: https://en.wikipedia.org/wiki/Ising_model
+  */
+  class NearestNeighborIsingModel1:
+    public Function {
+
+  private:
+
+    friend class boost::serialization::access;
+
+    /// Serialize
+    template<class Archive>
+    void serialize(Archive& ar, const unsigned int version)
+    {
+      ar & _couplings;
+      ar & _field;
+      ar & _periodic_boundary_conditions;
+    }
+
+    /// Couplings between nearest neighbors
+    std::vector<double> _couplings;
+
+    /// External field
+    std::vector<double> _field;
+
+    /// Flipped bits
+    bit_vector_t _flipped_bits;
+
+    /// Periodic boundary conditions
+    bool _periodic_boundary_conditions = false;
+
+    /// Resize data structures
+    void resize(int n);
+
+  public:
+
+    /// Constructor
+    NearestNeighborIsingModel1() {}
+
+    /** Random instance.
+
+        The weights are sampled from the normal distribution.
+
+        \param n Size of bit vector
+    */
+    void random(int n);
+
+    /** Random instance.
+
+        \param n Size of bit vectors
+        \param generator_couplings Couplings generator
+        \param generator_field External field generator
+    */
+    template<class Generator>
+    void random(int n, Generator generator_couplings, Generator generator_field) {
+      assert(n > 0);
+
+      resize(n);
+      for (size_t i = 0; i < _couplings.size(); i++) {
+        _couplings[i] = generator_couplings();
+        _field[i] = generator_field();
+      }
+    }
+
+    /** @name Evaluation
+     */
+    ///@{
+
+    /** Evaluate a bit vector.
+
+        Complexity: O(n)
+    */
+    double eval(const bit_vector_t&);
+
+    /// Incremental evaluation
+    double incremental_eval(const bit_vector_t& x, double v, const sparse_bit_vector_t& flipped_bits);
+
+    ///@}
+
+    /** @name Information about the function
+     */
+    ///@{
+
+    /// Get bit vector size
+    size_t get_bv_size() { return _couplings.size(); }
+
+    /** Check whether the function provides incremental evaluation.
+        \return true
+    */
+    bool provides_incremental_evaluation() { return true; }
+
+    ///@}
+
+    /// Set periodic boundary conditions
+    void set_periodic_boundary_conditions(bool x) { _periodic_boundary_conditions = x; }
+
+    /// Display
+    void display(std::ostream& stream);
+
+  };
+
+
+} // end of namespace function
+} // end of namespace hnco
+
+
+#endif
