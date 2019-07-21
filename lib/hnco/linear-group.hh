7f9ec414da75e35be38b64d0a2a84d3d08d3a870
Arnaud Berny <hnco@courros.fr>
2019-07-21 12:28:55 +0200

    Add gl_element_t

--- lib/hnco/linear-group.hh
+++ lib/hnco/linear-group.hh
@@ -18,146 +18,58 @@
 
 */
 
-#ifndef HNCO_BIT_MATRIX_H
-#define HNCO_BIT_MATRIX_H
-
-#include <algorithm>            // std::for_each
+#ifndef HNCO_LINEAR_GROUP_H
+#define HNCO_LINEAR_GROUP_H
 
 #include "bit-vector.hh"
 
 
 namespace hnco {
 
-  /** @name Types and functions related to bit matrices
+  /** @name Types and functions related to the linear group
    */
   ///@{
 
-  /// Bit matrix
-  typedef std::vector<bit_vector_t> bit_matrix_t;
+  /// Generator of the linear group
+  struct GlGenerator {
 
-  /// Display bit matrix
-  void bm_display(const bit_matrix_t& M, std::ostream& stream);
+    /// Types of generator
+    enum class Type { Permutation, Transvection };
 
-  /// Check whether a bit matrix is valid
-  bool bm_is_valid(const bit_matrix_t& M);
+    /// Type of generator
+    Type type;
 
-  /// Number of rows
-  inline int bm_num_rows(const bit_matrix_t& M) { return M.size(); }
+    /// First index
+    int first_index;
 
-  /// Number of columns
-  inline int bm_num_columns(const bit_matrix_t& M) { return M[0].size(); }
+    /// Second index
+    int second_index;
+  };
 
-  /// Check whether the matrix is a square matrix
-  inline bool bm_is_square(const bit_matrix_t& M) { return bm_num_rows(M) == bm_num_columns(M); }
+  /** Element of the linear group.
 
-  /// Check whether the matrix is the identity matrix
-  bool bm_is_identity(const bit_matrix_t& M);
+      The linear group on the finite field F_2 (GL for short) is the
+      set of invertible n by n bit matrices.
 
-  /// Check whether the matrix is upper triangular
-  bool bm_is_upper_triangular(const bit_matrix_t& M);
-
-  /// Resize a bit matrix
-  void bm_resize(bit_matrix_t& M, int num_rows, int num_columns);
-
-  /// Resize a bit matrix and make it a square matrix
-  inline void bm_resize(bit_matrix_t& M, int num_rows) { bm_resize(M, num_rows, num_rows); }
-
-  /// Clear bit matrix
-  inline void bm_clear(bit_matrix_t& M) { std::for_each(M.begin(), M.end(), [](bit_vector_t& row){ bv_clear(row); }); }
-
-  /** Set the matrix to the identity matrix.
-
-      \pre bm_is_square(M)
+      A GL element is encoded as a sequence of generators.
   */
-  void bm_identity(bit_matrix_t& M);
+  typedef std::vector<GlGenerator> gl_element_t;
 
-  /// Sample a random bit matrix
-  void bm_random(bit_matrix_t& M);
+  /// Display a GL element
+  void gl_display(const gl_element_t& M, std::ostream& stream);
 
-  /// Swap two rows
-  void bm_swap_rows(bit_matrix_t& M, int i, int j);
+  /** Sample a random GL element.
 
-  /** Add two rows.
-
-      Row i is added to row j.
+      \param M GL element
+      \param t Length of the generator sequence
   */
-  void bm_add_rows(bit_matrix_t& M, int i, int j);
+  void gl_random(gl_element_t& M, int t);
 
-  /** Compute a row echelon form of a matrix.
-
-      \warning A is modified by the function.
-  */
-  void bm_row_echelon_form(bit_matrix_t& A);
-
-  /** Compute the rank of a matrix.
-
-      \pre A must be in row echelon form.
-  */
-  int bm_rank(const bit_matrix_t& A);
-
-  /** Solve a linear system.
-
-      Solve the linear equation Ax = b.
-
-      \param A Matrix
-      \param b Right hand side
-
-      \pre bm_is_square(A)
-      \pre bm_num_rows(A) == b.size()
-
-      \return true if the system has a unique solution
-
-      \warning Both A and b are modified by the function. Provided
-      that A is invertible, after returning from the function, A is
-      the identity matrix and b is the unique solution to the linear
-      equation.
-  */
-  bool bm_solve(bit_matrix_t& A, bit_vector_t& b);
-
-  /** Solve a linear system in upper triangular form.
-
-      Solve the linear equation Ax = b.
-
-      \param A Upper triangular matrix
-      \param b Right hand side
-
-      \pre bm_is_square(A)
-      \pre bm_num_rows(A) == b.size()
-      \pre bm_is_upper_triangular(A)
-
-      \return true if the system has a unique solution
-
-      \warning Both A and b are modified by the function. Provided
-      that A is invertible, after returning from the function, A is
-      the identity matrix and b is the unique solution to the linear
-      equation.
-  */
-  bool bm_solve_upper_triangular(bit_matrix_t& A, bit_vector_t& b);
-
-  /** Invert a bit matrix.
-
-      \param M input matrix
-      \param N inverse matrix
-
-      \pre bm_is_square(M)
-      \pre bm_is_square(N)
-
-      \return true if M is invertible
-
-      \warning M is modified by the function. Provided that M is
-      invertible, after returning from the function, M is the identity
-      matrix and N is the computed inverse matrix.
-  */
-  bool bm_invert(bit_matrix_t& M, bit_matrix_t& N);
-
-  /** Multiply a bit matrix and a bit vector.
+  /** Multiply a GL element and a bit vector.
 
       The result is y = Mx.
   */
-  void bm_multiply(const bit_matrix_t& M, const bit_vector_t& x, bit_vector_t& y);
-
-  /// Transpose
-  void bm_transpose(const bit_matrix_t& M, bit_matrix_t& N);
+  void gl_multiply(const gl_element_t& M, const bit_vector_t& x, bit_vector_t& y);
 
   ///@}
 
