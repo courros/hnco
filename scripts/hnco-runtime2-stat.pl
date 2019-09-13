c2ae6c90f59676b255f138bf439fcfdb1e2fd19e
Arnaud Berny <hnco@courros.fr>
2019-09-13 19:11:41 +0200

    Generate document structure

--- scripts/hnco-runtime2-stat.pl
+++ scripts/hnco-runtime2-stat.pl
@@ -38,43 +38,50 @@ if (@ARGV) {
 print "Using $plan\n";
 
 open(FILE, $plan)
-    or die "hnco-runtime-stat.pl: Cannot open $plan\n";
+    or die "hnco-runtime2-stat.pl: Cannot open $plan\n";
 my $json = "";
 while (<FILE>) { $json .= $_; }
 
 my $obj = from_json($json);
 
 my $algorithms          = $obj->{algorithms};
-my $functions           = $obj->{functions};
-my $num_runs            = $obj->{num_runs};
-my $parameter           = $obj->{parameter};
+my $budget              = $obj->{budget};
+my $function            = $obj->{function};
+my $parallel            = $obj->{parallel};
+my $parameter1          = $obj->{parameter1};
+my $parameter2          = $obj->{parameter2};
+my $servers             = $obj->{servers};
 
-my $parameter_id        = $parameter->{id};
-my $boxwidth            = $parameter->{boxwidth};
+my $parameter1_id       = $parameter1->{id};
+my $boxwidth1           = $parameter1->{boxwidth};
+
+my $parameter2_id       = $parameter2->{id};
+my $boxwidth2           = $parameter2->{boxwidth};
+
+my $parameter1_values;
+if ($parameter1->{values_perl}) {
+    my @tmp = eval $parameter1->{values_perl};
+    $parameter1_values = \@tmp;
+} else {
+    $parameter1_values = $parameter1->{values};
+}
+
+my $parameter2_values;
+if ($parameter2->{values_perl}) {
+    my @tmp = eval $parameter2->{values_perl};
+    $parameter2_values = \@tmp;
+} else {
+    $parameter2_values = $parameter2->{values};
+}
 
 my $path_graphics       = "graphics";
 my $path_report         = "report";
 my $path_results        = "results";
-
-my $values;
-if ($parameter->{values_perl}) {
-    my @tmp = eval $parameter->{values_perl};
-    $values = \@tmp;
-} else {
-    $values = $parameter->{values};
-}
-
-my $all_stat = {};
+my $path_stats          = "results";
 
 unless (-d "$path_graphics") { mkdir "$path_graphics"; }
 
-add_missing_names($functions);
 add_missing_names($algorithms);
-compute_statistics();
-generate_data();
-generate_gnuplot_candlesticks();
-generate_gnuplot_mean();
-generate_gnuplot_stddev();
 generate_latex();
 
 sub add_missing_names
@@ -87,339 +94,17 @@ sub add_missing_names
     }
 }
 
-sub compute_statistics
-{
-    foreach my $f (@$functions) {
-        my $function_id = $f->{id};
-        my $function_stat = {};
-
-        foreach my $a (@$algorithms) {
-            my $algorithm_id = $a->{id};
-            my $algorithm_stat = {};
-            my $algorithm_num_runs = $num_runs;
-            if ($a->{deterministic}) {
-                $algorithm_num_runs = 1;
-            }
-
-            foreach my $value (@$values) {
-                my $prefix = "$path_results/$function_id/$algorithm_id/$parameter_id-$value";
-                my $SD = Statistics::Descriptive::Full->new();
-
-                foreach (1 .. $algorithm_num_runs) {
-                    my $path = "$prefix/$_.out";
-                    if (-f $path) {
-                        my $obj = from_json(read_file($path));
-                        $SD->add_data($obj->{total_num_evaluations});
-                    } else {
-                        die "hnco-runtime-stat.pl: compute_statistics: Cannot open '$path': $!\n";
-                    }
-                }
-
-                $algorithm_stat->{$value} = { min       => $SD->min(),
-                                              q1        => $SD->quantile(1),
-                                              median    => $SD->median(),
-                                              q3        => $SD->quantile(3),
-                                              max       => $SD->max(),
-                                              mean      => $SD->mean(),
-                                              stddev    => $SD->standard_deviation() };
-
-            }
-
-            $function_stat->{$algorithm_id} = $algorithm_stat;
-        }
-
-        $all_stat->{$function_id} = $function_stat;
-
-    }
-
-}
-
-sub generate_data
-{
-    foreach my $f (@$functions) {
-        my $function_id = $f->{id};
-
-        foreach my $a (@$algorithms) {
-            my $algorithm_id = $a->{id};
-            my $prefix = "$path_results/$function_id/$algorithm_id";
-
-            my $path = "$prefix/mean.dat";
-            my $mean = IO::File->new($path, '>')
-                or die "hnco-runtime-stat.pl: generate_data: Cannot open '$path': $!\n";
-
-            $path = "$prefix/quartiles.dat";
-            my $quartiles = IO::File->new($path, '>')
-                or die "hnco-runtime-stat.pl: generate_data: Cannot open '$path': $!\n";
-
-            foreach my $value (@$values) {
-                my $stat = $all_stat->{$function_id}->{$algorithm_id}->{$value};
-                $quartiles->printf("%e %e %e %e %e %e\n",
-                                   $value,
-                                   $stat->{min},
-                                   $stat->{q1},
-                                   $stat->{median},
-                                   $stat->{q3},
-                                   $stat->{max});
-                $mean->printf("%e %e %e\n",
-                              $value,
-                              $stat->{mean},
-                              $stat->{stddev});
-            }
-
-            $quartiles->close();
-            $mean->close();
-
-        }
-
-    }
-
-}
-
-sub generate_gnuplot_candlesticks
-{
-    open(CANDLESTICKS, ">candlesticks.gp")
-        or die "hnco-runtime-stat.pl: generate_gnuplot_candlesticks: Cannot open candlesticks.gp\n";
-
-    print CANDLESTICKS
-        "#!/usr/bin/gnuplot -persist\n",
-        "set grid\n",
-        "set xlabel \"$parameter_id\"\n",
-        "set ylabel \"Runtime\"\n",
-        "set autoscale fix\n",
-        "set offsets graph 0.05, graph 0.05, graph 0.05, graph 0.05\n\n";
-
-    if ($parameter->{logscale}) {
-        my $fmt = quote("10^{\%T}");
-        print CANDLESTICKS
-            "set logscale x\n",
-            "set format x $fmt\n";
-    }
-
-    foreach my $f (@$functions) {
-        my $function_id = $f->{id};
-        unless (-d "$path_graphics/$function_id") {
-            mkdir "$path_graphics/$function_id";
-        }
-
-        if ($f->{logscale}) {
-            my $fmt = quote("10^{\%T}");
-            print CANDLESTICKS
-                "set logscale y 10\n",
-                "set format y $fmt\n";
-        } else {
-            print CANDLESTICKS
-                "unset logscale y\n",
-                "set format y\n";
-        }
-
-        foreach my $a (@$algorithms) {
-            my $algorithm_id = $a->{id};
-            my $quoted_string = quote("$a->{name} on $f->{name}");
-            print CANDLESTICKS "set title $quoted_string\n";
-
-            $quoted_string = quote("$path_graphics/$function_id/$algorithm_id.pdf");
-            print CANDLESTICKS
-                $terminal{pdf}, "\n",
-                "set output $quoted_string\n";
-            $quoted_string = quote("$path_results/$function_id/$algorithm_id/quartiles.dat");
-            print CANDLESTICKS
-                "plot $quoted_string using 1:3:2:6:5:($boxwidth) with candlesticks lw 2 lt 3 notitle whiskerbars, \\\n",
-                "     $quoted_string using 1:4:4:4:4:($boxwidth) with candlesticks lw 2 lt 1 notitle\n";
-
-            $quoted_string = quote("$path_graphics/$function_id/$algorithm_id.eps");
-            print CANDLESTICKS
-                $terminal{eps}, "\n",
-                "set output $quoted_string\n",
-                "replot\n";
-
-            $quoted_string = quote("$path_graphics/$function_id/$algorithm_id.png");
-            print CANDLESTICKS
-                $terminal{png}, "\n",
-                "set output $quoted_string\n",
-                "replot\n\n";
-
-        }
-    }
-
-    system("chmod a+x candlesticks.gp");
-
-}
-
-sub generate_gnuplot_mean
-{
-    open(MEAN, ">mean.gp")
-        or die "hnco-runtime-stat.pl: generate_gnuplot_mean: Cannot open mean.gp\n";
-
-    print MEAN
-        "#!/usr/bin/gnuplot -persist\n",
-        "set grid\n",
-        "set xlabel \"$parameter_id\"\n",
-        "set ylabel \"Mean runtime\"\n",
-        "set logscale y\n",
-        "set format y", quote("10^{\%T}"), "\n",
-        "set key bottom right box opaque\n",
-        "set autoscale fix\n",
-        "set offsets graph 0.05, graph 0.05, graph 0.05, graph 0.05\n\n";
-
-    my $xmin = min(@$values);
-    my $xmax = max(@$values);
-
-    foreach my $f (@$functions) {
-        my $function_id = $f->{id};
-        unless (-d "$path_graphics/$function_id") { mkdir "$path_graphics/$function_id"; }
-        $quoted_string = quote("$f->{name}: Mean runtime as a function of $parameter_id");
-        print MEAN "set title $quoted_string\n";
-        foreach my $gnuplot (@{$f->{mean_gnuplot}}) {
-            print MEAN $gnuplot->{expression}, "\n";
-        }
-
-        my $quoted_string = quote("$path_graphics/$function_id/mean.pdf");
-        print MEAN
-            $terminal{pdf}, "\n",
-            "set output $quoted_string\n";
-
-        print MEAN "plot \\\n";
-        print MEAN
-            join ", \\\n",
-            (map {
-                my $algorithm_id = $_->{id};
-                my $name = quote($_->{name});
-                my $quoted_path = quote("$path_results/$function_id/$algorithm_id/mean.dat");
-                "  $quoted_path using 1:2 with l lw 2 title $name";
-             } @$algorithms);
-
-        foreach my $gnuplot (@{ $f->{mean_gnuplot}}) {
-            my ($before, $after) = split /\s*=\s*/, $gnuplot->{expression};
-            my $title = quote($gnuplot->{title});
-            print MEAN
-                ", \\\n",
-                "  [$xmin:$xmax] $before w l lw 2 title $title";
-        }
-        print MEAN "\n";
-
-        $quoted_path = quote("$path_graphics/$function_id/mean.eps");
-        print MEAN
-            $terminal{eps}, "\n",
-            "set output $quoted_path\n",
-            "replot\n";
-
-        $quoted_path = quote("$path_graphics/$function_id/mean.png");
-        print MEAN
-            $terminal{png}, "\n",
-            "set output $quoted_path\n",
-            "replot\n\n";
-
-    }
-
-    close(MEAN);
-
-    system("chmod a+x mean.gp");
-
-}
-
-sub generate_gnuplot_stddev
-{
-    open(STDDEV, ">stddev.gp")
-        or die "hnco-runtime-stat.pl: generate_gnuplot_stddev: Cannot open stddev.gp\n";
-
-    print STDDEV
-        "#!/usr/bin/gnuplot -persist\n",
-        "set grid\n",
-        "set xlabel \"$parameter_id\"\n",
-        "set ylabel \"Standard deviation of runtime\"\n",
-        "set logscale y\n",
-        "set format y", quote("10^{\%T}"), "\n",
-        "set key bottom right box opaque\n",
-        "set autoscale fix\n",
-        "set offsets graph 0.05, graph 0.05, graph 0.05, graph 0.05\n\n";
-
-    my $xmin = min(@$values);
-    my $xmax = max(@$values);
-
-    foreach my $f (@$functions) {
-        my $function_id = $f->{id};
-        unless (-d "$path_graphics/$function_id") { mkdir "$path_graphics/$function_id"; }
-        $quoted_string = quote("$f->{name}: Standard deviation of runtime as a function of $parameter_id");
-        print STDDEV "set title $quoted_string\n";
-        foreach my $gnuplot (@{$f->{stddev_gnuplot}}) {
-            print STDDEV $gnuplot->{expression}, "\n";
-        }
-
-        my $quoted_string = quote("$path_graphics/$function_id/stddev.pdf");
-        print STDDEV
-            $terminal{pdf}, "\n",
-            "set output $quoted_string\n";
-
-        print STDDEV "plot \\\n";
-        print STDDEV
-            join ", \\\n",
-            (map {
-                my $algorithm_id = $_->{id};
-                my $name = quote($_->{name});
-                my $quoted_path = quote("$path_results/$function_id/$algorithm_id/mean.dat");
-                "  $quoted_path using 1:3 with l lw 2 title $name";
-             } @$algorithms);
-
-        foreach my $gnuplot (@{ $f->{stddev_gnuplot}}) {
-            my ($before, $after) = split /\s*=\s*/, $gnuplot->{expression};
-            my $title = quote($gnuplot->{title});
-            print STDDEV
-                ", \\\n",
-                "  [$xmin:$xmax] $before w l lw 2 title $title";
-        }
-        print STDDEV "\n";
-
-        $quoted_path = quote("$path_graphics/$function_id/stddev.eps");
-        print STDDEV
-            $terminal{eps}, "\n",
-            "set output $quoted_path\n",
-            "replot\n";
-
-        $quoted_path = quote("$path_graphics/$function_id/stddev.png");
-        print STDDEV
-            $terminal{png}, "\n",
-            "set output $quoted_path\n",
-            "replot\n\n";
-
-    }
-
-    close(STDDEV);
-    system("chmod a+x stddev.gp");
-
-}
-
 sub generate_latex
 {
     open(LATEX, ">$path_report/results.tex")
-        or die "hnco-runtime-stat.pl: generate_latex: Cannot open $path_report/results.tex\n";
+        or die "hnco-runtime2-stat.pl: generate_latex: Cannot open $path_report/results.tex\n";
 
     print LATEX "\\graphicspath{{../$path_graphics/}}\n";
     latex_empty_line();
-
-    foreach my $f (@$functions) {
-        my $function_id = $f->{id};
-
-        latex_section("Function $f->{name}");
-
-        latex_begin_center();
-        latex_includegraphics("$function_id/mean");
-        latex_end_center();
-
-        latex_begin_center();
-        latex_includegraphics("$function_id/stddev");
-        latex_end_center();
-
-        foreach my $a (@$algorithms) {
-            my $algorithm_id = $a->{id};
-
-            latex_begin_center();
-            latex_includegraphics("$function_id/$algorithm_id");
-            latex_end_center();
-
-        }
-
+    foreach my $a (@$algorithms) {
+        my $algorithm_id = $a->{id};
+        latex_section("Algorithm $a->{name}");
     }
-
 }
 
 sub latex_section
@@ -500,7 +185,7 @@ sub read_file
     my $json;
     {
         local $/;
-        open my $fh, '<', $path or die "hnco-runtime-stat.pl: compute_statistics: Cannot open '$path': $!\n";
+        open my $fh, '<', $path or die "hnco-runtime2-stat.pl: compute_statistics: Cannot open '$path': $!\n";
         $json = <$fh>;
     }
     return $json;
