664e677223653e6ce8a45753337f77def988ad1e
Arnaud Berny <hnco@courros.fr>
2020-05-01 16:57:53 +0200

    Document namespaces

--- lib/hnco/functions/decorators/function-controller.hh
+++ lib/hnco/functions/decorators/function-controller.hh
@@ -33,6 +33,7 @@
 
 namespace hnco {
 namespace function {
+/// Controllers
 namespace controller {
 
 
--- lib/hnco/functions/decorators/function-modifier.hh
+++ lib/hnco/functions/decorators/function-modifier.hh
@@ -32,6 +32,7 @@
 
 namespace hnco {
 namespace function {
+/// Modifiers
 namespace modifier {
 
 
