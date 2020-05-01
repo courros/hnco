87b0696824bda2421b7994feece0208a7ee28d9d
Arnaud Berny <hnco@courros.fr>
2020-05-01 17:03:18 +0200

    Move files under functions/controllers

--- lib/hnco/functions/decorators/all.hh
+++ lib/hnco/functions/decorators/all.hh
@@ -21,8 +21,6 @@
 #ifndef HNCO_FUNCTIONS_DECORATORS_ALL_H
 #define HNCO_FUNCTIONS_DECORATORS_ALL_H
 
-#include "function-decorator.hh"
-#include "function-controller.hh"
 #include "function-modifier.hh"
 #include "prior-noise.hh"
 #include "parsed-modifier.hh"
