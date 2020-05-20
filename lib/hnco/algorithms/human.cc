55b3bfe619a3047623c1069277612aa337cc8090
Arnaud Berny <hnco@courros.fr>
2020-05-20 11:53:22 +0200

    Add iles for Human

---
+++ lib/hnco/algorithms/human.cc
@@ -0,0 +1,32 @@
+/* Copyright (C) 2016, 2017, 2018, 2019, 2020 Arnaud Berny
+
+   This file is part of HNCO.
+
+   HNCO is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   HNCO is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+   Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with HNCO. If not, see
+   <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "random-search.hh"
+
+
+using namespace hnco::algorithm;
+
+
+void
+RandomSearch::iterate()
+{
+  bv_random(_candidate);
+  update_solution(_candidate);
+}
---
+++ lib/hnco/algorithms/human.hh
@@ -0,0 +1,55 @@
+/* Copyright (C) 2016, 2017, 2018, 2019, 2020 Arnaud Berny
+
+   This file is part of HNCO.
+
+   HNCO is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   HNCO is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+   Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with HNCO. If not, see
+   <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef HNCO_ALGORITHMS_RANDOM_SEARCH_H
+#define HNCO_ALGORITHMS_RANDOM_SEARCH_H
+
+#include "hnco/algorithms/iterative-algorithm.hh"
+
+
+namespace hnco {
+namespace algorithm {
+
+  /// Random search
+  class RandomSearch:
+    public IterativeAlgorithm {
+
+    /// Candidate
+    bit_vector_t _candidate;
+
+  protected:
+
+    /// Single iteration
+    void iterate();
+
+  public:
+
+    /// Constructor
+    RandomSearch(int n):
+      IterativeAlgorithm(n),
+      _candidate(n) {}
+
+  };
+
+} // end of namespace algorithm
+} // end of namespace hnco
+
+
+#endif
