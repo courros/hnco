0f2ac328d5b473121f3f39e52db94fa784c421b7
Arnaud Berny <hnco@courros.fr>
2020-05-23 16:21:11 +0200

    Add file

---
+++ lib/hnco/algorithms/solution.hh
@@ -0,0 +1,184 @@
+/* Copyright (C) 2016, 2017, 2018, 2019, 2020 Arnaud Berny
+
+   This file is part of HNCO.
+
+   HNCO is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   HNCO is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+   Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with HNCO. If not, see
+   <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef HNCO_ALGORITHMS_ALGORITHM_H
+#define HNCO_ALGORITHMS_ALGORITHM_H
+
+#include <iostream>
+
+#include "hnco/exception.hh"
+#include "hnco/functions/function.hh"
+
+#include "log-context.hh"
+
+
+namespace hnco {
+/// Algorithms
+namespace algorithm {
+
+  /** Abstract search algorithm.
+
+      All algorithms maximize some given function, sometimes called a
+      fitness function or an objective function.
+  */
+  class Algorithm {
+
+  protected:
+
+    /// Function
+    function::Function *_function;
+
+    /** Functions.
+
+        Each thread has its own function.
+    */
+    std::vector<function::Function *> _functions;
+
+    /// Solution
+    point_value_t _solution;
+
+    /// Log context
+    LogContext *_log_context = nullptr;
+
+
+    /** @name Parameters
+     */
+    ///@{
+
+    /// Output stream
+    std::ostream *_stream = &std::cout;
+
+    ///@}
+
+
+    /** @name Managing solution
+     */
+    ///@{
+
+    /// Random solution
+    void random_solution();
+
+    /// Set solution
+    void set_solution(const bit_vector_t& x, double value);
+
+    /** Set solution.
+
+        \warning Evaluates the function once.
+    */
+    void set_solution(const bit_vector_t& x);
+
+    /// Update solution (strict)
+    void update_solution(const bit_vector_t& x, double value);
+
+    /// Update solution (strict)
+    void update_solution(const point_value_t& pv);
+
+    /** Update solution (strict).
+
+        \warning Evaluates the function once.
+    */
+    void update_solution(const bit_vector_t& x);
+
+    ///@}
+
+  public:
+
+    /// Constructor
+    Algorithm(int n):
+      _solution(bit_vector_t(n), 0.0)
+    {
+      if (!(n > 0))
+        throw hnco::exception::Error("Algorithm::Algorithm: bit vector size must be positive");
+    }
+
+    /// Destructor
+    virtual ~Algorithm() {}
+
+
+    /** @name Optimization
+     */
+    ///@{
+
+    /** Initialization.
+
+        Does nothing.
+
+        It is usually overridden by classes derived from
+        IterativeAlgorithm.
+    */
+    virtual void init() {}
+
+    /// Maximize
+    virtual void maximize() = 0;
+
+    /** Finalize.
+
+        Does nothing.
+
+        It is usually overridden by algorithms which do not keep
+        _solution up-to-date. In case _function throws a
+        LastEvaluation exception, the algorithm might leave _solution
+        in an undefined state. This can be fixed in this member
+        function.
+    */
+    virtual void finalize() {}
+
+    ///@}
+
+
+    /** @name Getters
+     */
+    ///@{
+
+    /// Get bit vector size
+    int get_bv_size() { return _solution.first.size(); }
+
+    /// Get the solution
+    const point_value_t& get_solution() { return _solution; }
+
+    ///@}
+
+
+    /** @name Setters
+     */
+    ///@{
+
+    /// Set the function
+    void set_function(function::Function *function) { _function = function; }
+
+    /// Set functions
+    void set_functions(const std::vector<function::Function *> functions) { _functions = functions; }
+
+    /// Set the output stream
+    void set_stream(std::ostream *stream) { _stream = stream; }
+
+    /// Set the log context
+    void set_log_context(LogContext *log_context) { _log_context = log_context; }
+
+    ///@}
+    
+  };
+
+
+} // end of namespace algorithm
+} // end of namespace hnco
+
+
+#endif
