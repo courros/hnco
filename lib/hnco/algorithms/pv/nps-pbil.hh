8f152e65467bbe1457077306332342343b43fa63
Arnaud Berny <hnco@courros.fr>
2020-06-20 18:30:30 +0200

    Format code

--- lib/hnco/algorithms/pv/nps-pbil.hh
+++ lib/hnco/algorithms/pv/nps-pbil.hh
@@ -29,41 +29,41 @@
 namespace hnco {
 namespace algorithm {
 
-  /** Population-based incremental learning with negative and positive selection.
+/** Population-based incremental learning with negative and positive selection.
 
-      Reference:
+    Reference:
 
-      Arnaud Berny. 2001. Extending selection learning toward
-      fixed-length d-ary strings. In Artificial Evolution (Lecture
-      Notes in Computer Science), P. Collet and others
-      (Eds.). Springer, Le Creusot.
+    Arnaud Berny. 2001. Extending selection learning toward
+    fixed-length d-ary strings. In Artificial Evolution (Lecture
+    Notes in Computer Science), P. Collet and others
+    (Eds.). Springer, Le Creusot.
 
-  */
-  class NpsPbil:
+*/
+class NpsPbil:
     public PvAlgorithm {
 
-  protected:
+protected:
 
-    /// Population
-    Population _population;
+  /// Population
+  Population _population;
 
-    /// Mean of best individuals
-    pv_t _mean_best;
+  /// Mean of best individuals
+  pv_t _mean_best;
 
-    /// Mean of worst individuals
-    pv_t _mean_worst;
+  /// Mean of worst individuals
+  pv_t _mean_worst;
 
-    /** @name Parameters
-     */
-    ///@{
+  /** @name Parameters
+   */
+  ///@{
 
-    /// Selection size
-    int _selection_size = 1;
+  /// Selection size
+  int _selection_size = 1;
 
-    /// Learning rate
-    double _learning_rate = 1e-3;
+  /// Learning rate
+  double _learning_rate = 1e-3;
 
-    ///@}
+  ///@}
 
   /** @name Loop
    */
@@ -77,28 +77,28 @@ namespace algorithm {
 
   ///@}
 
-  public:
+public:
 
-    /// Constructor
-    NpsPbil(int n, int population_size):
-      PvAlgorithm(n),
-      _population(population_size, n),
-      _mean_best(n),
-      _mean_worst(n) {}
+  /// Constructor
+  NpsPbil(int n, int population_size):
+    PvAlgorithm(n),
+    _population(population_size, n),
+    _mean_best(n),
+    _mean_worst(n) {}
 
-    /** @name Setters
-     */
-    ///@{
+  /** @name Setters
+   */
+  ///@{
 
-    /// Set the selection size
-    void set_selection_size(int x) { _selection_size = x; }
+  /// Set the selection size
+  void set_selection_size(int x) { _selection_size = x; }
 
-    /// Set the learning rate
-    void set_learning_rate(double x) { _learning_rate = x; }
+  /// Set the learning rate
+  void set_learning_rate(double x) { _learning_rate = x; }
 
-    ///@}
+  ///@}
 
-  };
+};
 
 } // end of namespace algorithm
 } // end of namespace hnco
