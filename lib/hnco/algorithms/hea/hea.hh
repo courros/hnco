2798a52b12ca5f383426d81c18a3ef36e049c1a0
Arnaud Berny <hnco@courros.fr>
2020-06-20 17:02:00 +0200

    Declare init private

--- lib/hnco/algorithms/hea/hea.hh
+++ lib/hnco/algorithms/hea/hea.hh
@@ -32,253 +32,260 @@
 
 namespace hnco {
 namespace algorithm {
+
 /// Herding evolutionary algorithm
 namespace hea {
 
 
-  /** Herding evolutionary algorithm.
+/** Herding evolutionary algorithm.
 
-      Reference:
+    Reference:
 
-      Arnaud Berny. 2015. Herding Evolutionary %Algorithm. In
-      Proceedings of the Companion Publication of the 2015 Annual
-      Conference on Genetic and Evolutionary Computation (GECCO
-      Companion ’15). ACM, New York, NY, USA, 1355–1356.
+    Arnaud Berny. 2015. Herding Evolutionary %Algorithm. In
+    Proceedings of the Companion Publication of the 2015 Annual
+    Conference on Genetic and Evolutionary Computation (GECCO
+    Companion ’15). ACM, New York, NY, USA, 1355–1356.
 
-  */
-  template<class Moment, class Herding>
-  class Hea:
-    public algorithm::IterativeAlgorithm {
+*/
+template<class Moment, class Herding>
+class Hea: public algorithm::IterativeAlgorithm {
 
-  public:
+public:
 
-    enum {
-      /// Log error
-      LOG_ERROR,
+  enum {
+        /// Log error
+        LOG_ERROR,
 
-      /// Log distance to uniform
-      LOG_DTU,
+        /// Log distance to uniform
+        LOG_DTU,
 
-      /// Log delta (moment increment)
-      LOG_DELTA,
+        /// Log delta (moment increment)
+        LOG_DELTA,
 
-      /// Log the distance between the target and the selection moment
-      LOG_SELECTION,
+        /// Log the distance between the target and the selection moment
+        LOG_SELECTION,
 
-      /// Log the moment matrix
-      LOG_MOMENT_MATRIX,
+        /// Log the moment matrix
+        LOG_MOMENT_MATRIX,
 
-      LAST_LOG
-    };
+        LAST_LOG
+  };
 
-    /// Type for log flags
-    typedef std::bitset<LAST_LOG> log_flags_t;
+  /// Type for log flags
+  typedef std::bitset<LAST_LOG> log_flags_t;
 
-  private:
+private:
 
-    /// Moment
-    Moment _target;
+  /// Moment
+  Moment _target;
 
-    /// Moment of selected individuals
-    Moment _selection;
+  /// Moment of selected individuals
+  Moment _selection;
 
-    /// Uniform moment
-    Moment _uniform;
+  /// Uniform moment
+  Moment _uniform;
 
-    /// Population
-    algorithm::Population _population;
+  /// Population
+  algorithm::Population _population;
 
-    /// Herding
-    Herding *_herding;
+  /// Herding
+  Herding *_herding;
 
-    /** @name Logging
-     */
-    ///@{
+  /** @name Logging
+   */
+  ///@{
 
-    /// Error cache
-    double _error_cache;
+  /// Error cache
+  double _error_cache;
 
-    /// Distance to uniform cache
-    double _dtu_cache;
+  /// Distance to uniform cache
+  double _dtu_cache;
 
-    /// Delta cache
-    double _delta_cache;
+  /// Delta cache
+  double _delta_cache;
 
-    /// Selection distance cache
-    double _selection_cache;
+  /// Selection distance cache
+  double _selection_cache;
 
-    /// Log flags
-    log_flags_t _log_flags;
+  /// Log flags
+  log_flags_t _log_flags;
 
-    ///@}
+  ///@}
 
-    /** @name Parameters
-     */
-    ///@{
+  /** @name Parameters
+   */
+  ///@{
 
-    /// Moment margin
-    double _margin;
+  /// Moment margin
+  double _margin;
 
-    /// Selection size
-    int _selection_size = 1;
+  /// Selection size
+  int _selection_size = 1;
 
-    /// Reset period
-    int _reset_period = 0;
+  /// Reset period
+  int _reset_period = 0;
 
-    /// Learning rate
-    double _learning_rate = 1e-4;
+  /// Learning rate
+  double _learning_rate = 1e-4;
 
-    /// Bound moment after update
-    bool _bound_moment = false;
+  /// Bound moment after update
+  bool _bound_moment = false;
 
-    ///@}
+  ///@}
 
-    /// Single iteration
-    void iterate() {
-      if (_reset_period > 0) {
-        if (_iteration % _reset_period == 0)
-          _herding->init();
-      }
+  /** @name Loop
+   */
+  ///@{
 
-      for (int i = 0; i < _population.size(); i++)
-        _herding->sample(_target, _population.get_bv(i));
+  /// Initialization
+  void init() override {
+    random_solution();
+    _target.uniform();
+    _herding->init();
+  }
 
-      if (_log_flags[LOG_ERROR])
-        _error_cache = _herding->error(_target);
-
-      if (_log_flags[LOG_DTU])
-        _dtu_cache = _target.distance(_uniform);
-
-      if (_log_flags[LOG_DELTA])
-        _delta_cache = _herding->get_delta().norm_2();
-
-      if (_functions.size() > 1)
-        _population.evaluate(_functions);
-      else
-        _population.evaluate(_function);
-
-      _population.sort();
-
-      update_solution(_population.get_best_bv(),
-                      _population.get_best_value());
-
-      _selection.init();
-      for (int i = 0; i < _selection_size; i++)
-        _selection.add(_population.get_best_bv(i));
-      if (_selection_size > 1)
-        _selection.average(_selection_size);
-
-      if (_log_flags[LOG_SELECTION])
-        _selection_cache = _target.distance(_selection);
-
-      _target.update(_selection, _learning_rate);
-
-      if (_bound_moment)
-        _target.bound(_margin);
-    }
-
-    /// Log
-    void log() {
-      assert(_log_flags.any());
-
-      if (_log_flags[LOG_MOMENT_MATRIX]) {
-        _target.display(*_stream);
-        return;
-      }
-
-      // Single line
-      if (_log_flags[LOG_ERROR])
-        (*_stream) << _error_cache << " ";
-
-      if (_log_flags[LOG_DTU])
-        (*_stream) << _dtu_cache << " ";
-
-      if (_log_flags[LOG_DELTA])
-        (*_stream) << _delta_cache << " ";
-
-      if (_log_flags[LOG_SELECTION])
-        (*_stream) << _selection_cache << " ";
-
-      (*_stream) << std::endl;
-    }
-
-  public:
-
-    /** Constructor.
-
-        \param n Size of bit vectors
-        \param population_size Population size
-
-        _margin is initialized to 1 / n.
-    */
-    Hea(int n, int population_size):
-      IterativeAlgorithm(n),
-      _target(n),
-      _selection(n),
-      _uniform(n),
-      _population(population_size, n),
-      _margin(1 / double(n))
-    {
-      _uniform.uniform();
-    }
-
-    /// Initialization
-    void init() {
-        random_solution();
-        _target.uniform();
+  /// Single iteration
+  void iterate() override {
+    if (_reset_period > 0) {
+      if (_iteration % _reset_period == 0)
         _herding->init();
     }
 
-    /** @name Setters
-     */
-    ///@{
+    for (int i = 0; i < _population.size(); i++)
+      _herding->sample(_target, _population.get_bv(i));
 
-    /// Set the herding algorithm
-    void set_herding(Herding *x) {
-      assert(x);
-      _herding = x;
+    if (_log_flags[LOG_ERROR])
+      _error_cache = _herding->error(_target);
+
+    if (_log_flags[LOG_DTU])
+      _dtu_cache = _target.distance(_uniform);
+
+    if (_log_flags[LOG_DELTA])
+      _delta_cache = _herding->get_delta().norm_2();
+
+    if (_functions.size() > 1)
+      _population.evaluate(_functions);
+    else
+      _population.evaluate(_function);
+
+    _population.sort();
+
+    update_solution(_population.get_best_bv(),
+                    _population.get_best_value());
+
+    _selection.init();
+    for (int i = 0; i < _selection_size; i++)
+      _selection.add(_population.get_best_bv(i));
+    if (_selection_size > 1)
+      _selection.average(_selection_size);
+
+    if (_log_flags[LOG_SELECTION])
+      _selection_cache = _target.distance(_selection);
+
+    _target.update(_selection, _learning_rate);
+
+    if (_bound_moment)
+      _target.bound(_margin);
+  }
+
+  /// Log
+  void log() override {
+    assert(_log_flags.any());
+
+    if (_log_flags[LOG_MOMENT_MATRIX]) {
+      _target.display(*_stream);
+      return;
     }
 
-    /// Set the moment margin
-    void set_margin(double x) { _margin = x; }
+    // Single line
+    if (_log_flags[LOG_ERROR])
+      (*_stream) << _error_cache << " ";
 
-    /** Set the selection size.
+    if (_log_flags[LOG_DTU])
+      (*_stream) << _dtu_cache << " ";
 
-        The selection size is the number of selected individuals in
-        the population.
-    */
-    void set_selection_size(int x) { _selection_size = x; }
+    if (_log_flags[LOG_DELTA])
+      (*_stream) << _delta_cache << " ";
 
-    /** Set the reset period
+    if (_log_flags[LOG_SELECTION])
+      (*_stream) << _selection_cache << " ";
 
-        \param x Reset period
+    (*_stream) << std::endl;
+  }
 
-        x <= 0 means no reset.
-    */
-    void set_reset_period(int x) { _reset_period = x; }
+  ///@}
 
-    /// Set the learning rate
-    void set_learning_rate(double x) { _learning_rate = x; }
+public:
 
-    /// Set the bound moment after update
-    void set_bound_moment(bool x) { _bound_moment = x; }
+  /** Constructor.
 
-    /// Set weight
-    void set_weight(double weight) {
-      _target._weight = weight;
-      _selection._weight = weight;
-      _uniform._weight = weight;
-    }
+      \param n Size of bit vectors
+      \param population_size Population size
 
-    /// Set log flags
-    void set_log_flags(const log_flags_t& lf) {
-      _log_flags = lf;
-      _something_to_log = _log_flags.any();
-    }
+      _margin is initialized to 1 / n.
+  */
+  Hea(int n, int population_size):
+    IterativeAlgorithm(n),
+    _target(n),
+    _selection(n),
+    _uniform(n),
+    _population(population_size, n),
+    _margin(1 / double(n))
+  {
+    _uniform.uniform();
+  }
 
-    ///@}
+  /** @name Setters
+   */
+  ///@{
 
-  };
+  /// Set the herding algorithm
+  void set_herding(Herding *x) {
+    assert(x);
+    _herding = x;
+  }
+
+  /// Set the moment margin
+  void set_margin(double x) { _margin = x; }
+
+  /** Set the selection size.
+
+      The selection size is the number of selected individuals in
+      the population.
+  */
+  void set_selection_size(int x) { _selection_size = x; }
+
+  /** Set the reset period
+
+      \param x Reset period
+
+      x <= 0 means no reset.
+  */
+  void set_reset_period(int x) { _reset_period = x; }
+
+  /// Set the learning rate
+  void set_learning_rate(double x) { _learning_rate = x; }
+
+  /// Set the bound moment after update
+  void set_bound_moment(bool x) { _bound_moment = x; }
+
+  /// Set weight
+  void set_weight(double weight) {
+    _target._weight = weight;
+    _selection._weight = weight;
+    _uniform._weight = weight;
+  }
+
+  /// Set log flags
+  void set_log_flags(const log_flags_t& lf) {
+    _log_flags = lf;
+    _something_to_log = _log_flags.any();
+  }
+
+  ///@}
+
+};
+
 
 } // end of namespace hea
 } // end of namespace algorithm
