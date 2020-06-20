e35461a1670546a3e850212b198e13131a5381ed
Arnaud Berny <hnco@courros.fr>
2020-06-20 18:29:17 +0200

    Indent code

--- lib/hnco/algorithms/pv/mmas.hh
+++ lib/hnco/algorithms/pv/mmas.hh
@@ -30,34 +30,34 @@ namespace hnco {
 namespace algorithm {
 
 
-  /** Max-min ant system.
+/** Max-min ant system.
 
-      Reference:
+    Reference:
 
-      Thomas Stützle and Holger H. Hoos. 2000. MAX–MIN Ant
-      System. Future Generation Computer Systems 16, 8 (2000),
-      889–914.
+    Thomas Stützle and Holger H. Hoos. 2000. MAX–MIN Ant
+    System. Future Generation Computer Systems 16, 8 (2000),
+    889–914.
 
-  */
-  class Mmas:
+*/
+class Mmas:
     public PvAlgorithm {
 
-  protected:
+protected:
 
-    /// Candidate solution
-    bit_vector_t _x;
+  /// Candidate solution
+  bit_vector_t _x;
 
-    /** @name Parameters
-     */
-    ///@{
+  /** @name Parameters
+   */
+  ///@{
 
-    /// Binary operator for comparing evaluations
-    std::function<bool(double, double)> _compare = std::greater_equal<double>();
+  /// Binary operator for comparing evaluations
+  std::function<bool(double, double)> _compare = std::greater_equal<double>();
 
-    /// Learning rate
-    double _learning_rate = 1e-3;
+  /// Learning rate
+  double _learning_rate = 1e-3;
 
-    ///@}
+  ///@}
 
   /** @name Loop
    */
@@ -71,26 +71,26 @@ namespace algorithm {
 
   ///@}
 
-  public:
+public:
 
-    /// Constructor
-    Mmas(int n):
-      PvAlgorithm(n),
-      _x(n) {}
+  /// Constructor
+  Mmas(int n):
+    PvAlgorithm(n),
+    _x(n) {}
 
-    /** @name Setters
-     */
-    ///@{
+  /** @name Setters
+   */
+  ///@{
 
-    /// Set the binary operator for comparing evaluations
-    void set_compare(std::function<bool(double, double)> x) { _compare = x; }
+  /// Set the binary operator for comparing evaluations
+  void set_compare(std::function<bool(double, double)> x) { _compare = x; }
 
-    /// Set the learning rate
-    void set_learning_rate(double x) { _learning_rate = x; }
+  /// Set the learning rate
+  void set_learning_rate(double x) { _learning_rate = x; }
 
-    ///@}
+  ///@}
 
-  };
+};
 
 
 } // end of namespace algorithm
