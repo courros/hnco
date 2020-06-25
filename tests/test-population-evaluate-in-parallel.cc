143539bbe16516c2940149e6e93c90abd0435b14
Arnaud Berny <hnco@courros.fr>
2020-06-25 20:09:22 +0200

    Implement test-population-evaluate-in-parallel

--- tests/test-population-evaluate-in-parallel.cc
+++ tests/test-population-evaluate-in-parallel.cc
@@ -24,37 +24,66 @@
 
 */
 
+#include <omp.h>                // omp_set_num_threads
+
 #include <iostream>
 
-#include "hnco/algorithms/population.hh"
 #include "hnco/functions/theory.hh"
-
+#include "hnco/map.hh"
+#include "hnco/functions/modifiers/modifier.hh"
+#include "hnco/algorithms/population.hh"
 
 using namespace hnco::random;
 using namespace hnco::algorithm;
+using namespace hnco::function;
+using namespace hnco::function::modifier;
 using namespace hnco;
 
 bool check()
 {
-  std::uniform_int_distribution<int> dist_population_size(2, 10);
-  std::uniform_int_distribution<int> dist_n(2, 10);
-  std::uniform_int_distribution<int> dist_num_threads(1, 5);
+  std::uniform_int_distribution<int> dist_population_size(1, 100);
+  std::uniform_int_distribution<int> dist_n(1, 100);
+  std::uniform_int_distribution<int> dist_num_threads(1, 100);
 
   for (int i = 0; i < 10; i++) {
     const int population_size   = dist_population_size  (Generator::engine);
     const int n                 = dist_n                (Generator::engine);
     const int num_threads       = dist_num_threads      (Generator::engine);
 
+    omp_set_num_threads(num_threads);
+
+    OneMax fn(n);
+
+    Translation translation;
+    translation.random(n);
+
+    std::vector<Function *> fns(num_threads);
+    for (auto& composition : fns) {
+      composition = new FunctionMapComposition(&fn, &translation);
+    }
+
     Population population_seq(population_size, n);
+    assert(population_seq.size() == population_size);
     population_seq.random();
 
     Population population_par(population_seq);
+    assert(population_seq.size() == population_par.size());
 
-    function::OneMax fn(n);
-    std::vector<function::Function *> fns(num_threads, &fn);
-
-    population_seq.evaluate(&fn);
+    population_seq.evaluate(fns[0]);
     population_par.evaluate_in_parallel(fns);
+
+    for (int j = 0; j < population_size; j++) {
+      assert(population_seq.get_bv(j) == population_par.get_bv(j));
+      // Populations are not sorted so get_best_value returns the
+      // value of get_bv with the same index.
+      if (population_seq.get_best_value(j) != population_par.get_best_value(j))
+        return false;
+    }
+
+    for (auto composition : fns) {
+      delete composition;
+    }
+
   }
 
   return true;
