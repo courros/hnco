f3a9f5b4971ac5d7a1231915fcd93aee8f14a54d
Arnaud Berny <hnco@courros.fr>
2020-06-25 18:19:59 +0200

    Indent code

--- lib/hnco/algorithms/population.hh
+++ lib/hnco/algorithms/population.hh
@@ -32,210 +32,210 @@ namespace hnco {
 namespace algorithm {
 
 
-  /// Population
-  class Population {
+/// Population
+class Population {
 
-  protected:
+protected:
 
-    /// Index-value type
-    typedef std::pair<int, double> index_value_t;
+  /// Index-value type
+  typedef std::pair<int, double> index_value_t;
 
-    /// Bit vectors
-    std::vector<bit_vector_t> _bvs;
+  /// Bit vectors
+  std::vector<bit_vector_t> _bvs;
 
-    /** Lookup table.
+  /** Lookup table.
 
-        Let p be of type std::pair<int, double>. Then p.first is the
-        bv index in the unsorted population whereas p.second is the bv
-        value.
-    */
-    std::vector<index_value_t> _lookup;
+      Let p be of type std::pair<int, double>. Then p.first is the
+      bv index in the unsorted population whereas p.second is the bv
+      value.
+  */
+  std::vector<index_value_t> _lookup;
 
-    /// Binary operator for comparing index-value pairs
-    std::function<bool(const index_value_t&, const index_value_t&)> _compare_index_value =
-      [](const index_value_t& a, const index_value_t& b) { return a.second > b.second; };
+  /// Binary operator for comparing index-value pairs
+  std::function<bool(const index_value_t&, const index_value_t&)> _compare_index_value =
+    [](const index_value_t& a, const index_value_t& b) { return a.second > b.second; };
 
-  public:
+public:
 
-    /// Constructor
-    Population(int population_size, int n):
-      _bvs(population_size, bit_vector_t(n)),
-      _lookup(population_size) {}
+  /// Constructor
+  Population(int population_size, int n):
+    _bvs(population_size, bit_vector_t(n)),
+    _lookup(population_size) {}
 
-    /// Size
-    int size() const { return _bvs.size(); }
+  /// Size
+  int size() const { return _bvs.size(); }
 
-    /// Initialize the population with random bit vectors
-    void random();
+  /// Initialize the population with random bit vectors
+  void random();
 
 
-    /** @name Get bit vectors for non const populations
-     */
-    ///@{
+  /** @name Get bit vectors for non const populations
+   */
+  ///@{
 
-    /// Get a bit vector
-    bit_vector_t& get_bv(int i) { return _bvs[i]; }
+  /// Get a bit vector
+  bit_vector_t& get_bv(int i) { return _bvs[i]; }
 
-    /** Get best bit vector.
+  /** Get best bit vector.
 
-        \pre The population must be sorted.
-    */
-    bit_vector_t& get_best_bv() { return _bvs[_lookup[0].first]; }
+      \pre The population must be sorted.
+  */
+  bit_vector_t& get_best_bv() { return _bvs[_lookup[0].first]; }
 
-    /** Get best bit vector.
+  /** Get best bit vector.
 
-        \param i Index in the sorted population
+      \param i Index in the sorted population
 
-        \pre The population must be sorted.
-    */
-    bit_vector_t& get_best_bv(int i) { return _bvs[_lookup[i].first]; }
+      \pre The population must be sorted.
+  */
+  bit_vector_t& get_best_bv(int i) { return _bvs[_lookup[i].first]; }
 
-    /** Get worst bit vector.
+  /** Get worst bit vector.
 
-        \param i Index in the sorted population
+      \param i Index in the sorted population
 
-        \pre The population must be sorted.
-    */
-    bit_vector_t& get_worst_bv(int i) { return get_best_bv(_bvs.size() - 1 - i); }
+      \pre The population must be sorted.
+  */
+  bit_vector_t& get_worst_bv(int i) { return get_best_bv(_bvs.size() - 1 - i); }
 
-    ///@}
+  ///@}
 
 
-    /** @name Get bit vectors for const populations
-     */
-    ///@{
+  /** @name Get bit vectors for const populations
+   */
+  ///@{
 
-    /// Get a bit vector
-    const bit_vector_t& get_bv(int i) const { return _bvs[i]; }
+  /// Get a bit vector
+  const bit_vector_t& get_bv(int i) const { return _bvs[i]; }
 
-    /** Get best bit vector.
+  /** Get best bit vector.
 
-        \pre The population must be sorted.
-    */
-    const bit_vector_t& get_best_bv() const { return _bvs[_lookup[0].first]; }
+      \pre The population must be sorted.
+  */
+  const bit_vector_t& get_best_bv() const { return _bvs[_lookup[0].first]; }
 
-    /** Get best bit vector.
+  /** Get best bit vector.
 
-        \param i Index in the sorted population
+      \param i Index in the sorted population
 
-        \pre The population must be sorted.
-    */
-    const bit_vector_t& get_best_bv(int i) const { return _bvs[_lookup[i].first]; }
+      \pre The population must be sorted.
+  */
+  const bit_vector_t& get_best_bv(int i) const { return _bvs[_lookup[i].first]; }
 
-    /** Get worst bit vector.
+  /** Get worst bit vector.
 
-        \param i Index in the sorted population
+      \param i Index in the sorted population
 
-        \pre The population must be sorted.
-    */
-    const bit_vector_t& get_worst_bv(int i) const { return get_best_bv(_bvs.size() - 1 - i); }
+      \pre The population must be sorted.
+  */
+  const bit_vector_t& get_worst_bv(int i) const { return get_best_bv(_bvs.size() - 1 - i); }
 
-    ///@}
+  ///@}
 
 
-    /** @name Get sorted values
-     */
-    ///@{
+  /** @name Get sorted values
+   */
+  ///@{
 
-    /** Get best value.
+  /** Get best value.
 
-        \param i Index in the sorted population
+      \param i Index in the sorted population
 
-        \pre The population must be sorted.
-    */
-    double get_best_value(int i) const { return _lookup[i].second; }
+      \pre The population must be sorted.
+  */
+  double get_best_value(int i) const { return _lookup[i].second; }
 
-    /** Get best value.
+  /** Get best value.
 
-        \pre The population must be sorted.
-    */
-    double get_best_value() const { return _lookup[0].second; }
+      \pre The population must be sorted.
+  */
+  double get_best_value() const { return _lookup[0].second; }
 
-    ///@}
+  ///@}
 
 
-    /** @name Evaluation and sorting
-     */
-    ///@{
+  /** @name Evaluation and sorting
+   */
+  ///@{
 
-    /// Evaluate the population
-    void evaluate(function::Function *function);
+  /// Evaluate the population
+  void evaluate(function::Function *function);
 
-    /// Evaluate the population in parallel
-    void evaluate_in_parallel(const std::vector<function::Function *>& functions);
+  /// Evaluate the population in parallel
+  void evaluate_in_parallel(const std::vector<function::Function *>& functions);
 
-    /// Sort the lookup table
-    void sort() { std::sort(_lookup.begin(), _lookup.end(), _compare_index_value); }
+  /// Sort the lookup table
+  void sort() { std::sort(_lookup.begin(), _lookup.end(), _compare_index_value); }
 
-    /// Partially sort the lookup table
-    void partial_sort(int selection_size) {
-      assert(selection_size > 0);
-      std::partial_sort(_lookup.begin(), _lookup.begin() + selection_size, _lookup.end(), _compare_index_value);
-    }
+  /// Partially sort the lookup table
+  void partial_sort(int selection_size) {
+    assert(selection_size > 0);
+    std::partial_sort(_lookup.begin(), _lookup.begin() + selection_size, _lookup.end(), _compare_index_value);
+  }
 
-    /// Shuffle the lookup table
-    void shuffle() { std::shuffle(_lookup.begin(), _lookup.end(), random::Generator::engine); }
+  /// Shuffle the lookup table
+  void shuffle() { std::shuffle(_lookup.begin(), _lookup.end(), random::Generator::engine); }
 
-    ///@}
+  ///@}
 
 
-    /** @name Selection
-     */
-    ///@{
+  /** @name Selection
+   */
+  ///@{
 
-    /** Plus selection.
+  /** Plus selection.
 
-        Implemented with a copy.
+      Implemented with a copy.
 
-        \pre Both populations must be completely sorted.
+      \pre Both populations must be completely sorted.
 
-        \warning The function does not break ties randomly
-        (workaround: shuffle parents and offsprings).
-    */
-    void plus_selection(const Population& offsprings);
+      \warning The function does not break ties randomly
+      (workaround: shuffle parents and offsprings).
+  */
+  void plus_selection(const Population& offsprings);
 
-    /** Plus selection.
+  /** Plus selection.
 
-        Implemented with a swap. Should be faster than plus_selection
-        with a copy.
+      Implemented with a swap. Should be faster than plus_selection
+      with a copy.
 
-        \pre Both populations must be completely sorted.
+      \pre Both populations must be completely sorted.
 
-        \warning The function does not break ties randomly
-        (workaround: shuffle parents and offsprings).
+      \warning The function does not break ties randomly
+      (workaround: shuffle parents and offsprings).
 
-        \warning Modifies its argument.
-    */
-    void plus_selection(Population& offsprings);
+      \warning Modifies its argument.
+  */
+  void plus_selection(Population& offsprings);
 
-    /** Comma selection.
+  /** Comma selection.
 
-        Implemented with a copy.
+      Implemented with a copy.
 
-        \pre Offspring population must be partially sorted.
+      \pre Offspring population must be partially sorted.
 
-        \warning The function does not break ties randomly
-        (workaround: shuffle offsprings).
-    */
-    void comma_selection(const Population& offsprings);
+      \warning The function does not break ties randomly
+      (workaround: shuffle offsprings).
+  */
+  void comma_selection(const Population& offsprings);
 
-    /** Comma selection.
+  /** Comma selection.
 
-        Implemented with a swap. Should be faster than comma_selection
-        with a copy.
+      Implemented with a swap. Should be faster than comma_selection
+      with a copy.
 
-        \pre Offspring population must be partially sorted.
+      \pre Offspring population must be partially sorted.
 
-        \warning The function does not break ties randomly
-        (workaround: shuffle offsprings).
+      \warning The function does not break ties randomly
+      (workaround: shuffle offsprings).
 
-        \warning Modifies its argument.
-    */
-    void comma_selection(Population& offsprings);
+      \warning Modifies its argument.
+  */
+  void comma_selection(Population& offsprings);
 
-    ///@}
+  ///@}
 
-  };
+};
 
 
 }
