3b5a68222c0303f5d64d7addb9bc5e4e5dfb8a36
Arnaud Berny <hnco@courros.fr>
2020-07-04 11:10:31 +0200

    Add files

---
+++ lib/hnco/log/logger.cc
@@ -0,0 +1,223 @@
+/* Copyright (C) 2016, 2017, 2018, 2019, 2020 Arnaud Berny
+
+   This file is part of HNCO.
+
+   HNCO is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   HNCO is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+   Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with HNCO. If not, see
+   <http://www.gnu.org/licenses/>.
+
+*/
+
+#include <assert.h>
+
+#include <string>               // std::getline
+
+#include "util.hh"              // hnco::have_same_size
+#include "bit-vector.hh"
+
+
+using namespace hnco;
+
+
+void check_libhnco(void) {}
+
+
+void
+hnco::bv_display(const bit_vector_t& v, std::ostream& stream)
+{
+  for (auto c : v)
+    if (c == 0)
+      stream << 0;
+    else
+      stream << 1;
+}
+
+int
+hnco::bv_hamming_weight(const std::vector<bool>& x)
+{
+  int result = 0;
+  for (std::size_t i = 0; i < x.size(); i++)
+    if (x[i])
+      result++;
+  return result;
+}
+
+int
+hnco::bv_hamming_distance(const bit_vector_t& x, const bit_vector_t& y)
+{
+  assert(x.size() == y.size());
+
+  int d = 0;
+  for (std::size_t i = 0; i < x.size(); i++)
+    if (x[i] != y[i])
+      d++;
+  return d;
+}
+
+bit_t
+hnco::bv_dot_product(const bit_vector_t& x, const bit_vector_t& y)
+{
+  assert(x.size() == y.size());
+
+  size_t result = 0;
+  for (std::size_t i = 0; i < x.size(); i++)
+    if (x[i] && y[i])
+      result++;
+  return result % 2;
+}
+
+bit_t
+hnco::bv_dot_product(const bit_vector_t& x, const std::vector<bool>& y)
+{
+  assert(x.size() == y.size());
+
+  size_t result = 0;
+  for (std::size_t i = 0; i < x.size(); i++)
+    if (x[i] && y[i])
+      result++;
+  return result % 2;
+}
+
+void
+hnco::bv_flip(bit_vector_t& x, const bit_vector_t& mask)
+{
+  assert(mask.size() == x.size());
+
+  for (std::size_t i = 0; i < x.size(); i++)
+    if (mask[i])
+      bv_flip(x, i);
+}
+
+void
+hnco::bv_random(bit_vector_t& x, int k)
+{
+  assert(k >= 0);
+  assert(k <= static_cast<int>(x.size()));
+
+  int n = x.size();
+  for (size_t i = 0; i < x.size(); i++) {
+    if (k > 0) {
+      assert(n > 0);
+      double p = double(k) / double(n);
+      if (random::Generator::uniform() < p) {
+        x[i] = 1;
+        k--;
+      } else
+        x[i] = 0;
+      n--;
+    } else
+      x[i] = 0;
+  }
+}
+
+void
+hnco::bv_add(bit_vector_t& dest, const bit_vector_t& src)
+{
+  assert(dest.size() == src.size());
+
+  for (std::size_t i = 0; i < dest.size(); i++)
+    dest[i] = (src[i] + dest[i]) % 2;
+}
+
+void
+hnco::bv_add(bit_vector_t& dest, const bit_vector_t& x, const bit_vector_t& y)
+{
+  assert(dest.size() == x.size());
+  assert(dest.size() == y.size());
+
+  for (std::size_t i = 0; i < dest.size(); i++)
+    dest[i] = (x[i] + y[i]) % 2;
+}
+
+void
+hnco::bv_to_vector_bool(std::vector<bool>& y, const bit_vector_t& x)
+{
+  assert(y.size() == x.size());
+
+  for (size_t i = 0; i < x.size(); i++) {
+    if (x[i])
+      y[i] = true;
+    else
+      y[i] = false;
+  }
+}
+
+void
+hnco::bv_from_vector_bool(bit_vector_t& x, const std::vector<bool>& y)
+{
+  assert(y.size() == x.size());
+
+  for (size_t i = 0; i < x.size(); i++) {
+    if (y[i])
+      x[i] = 1;
+    else
+      x[i] = 0;
+  }
+}
+
+std::size_t
+hnco::bv_to_size_type(const bit_vector_t& x)
+{
+  assert(x.size() <= 8 * sizeof(std::size_t));
+
+  std::size_t result = 0;
+  std::size_t power = 1;
+  for (size_t i = 0; i < x.size(); i++) {
+    if (x[i])
+      result |= power;
+    power <<= 1;
+  }
+  return result;
+}
+
+void
+hnco::bv_from_size_type(bit_vector_t& x, std::size_t u)
+{
+  assert(x.size() <= 8 * sizeof(std::size_t));
+
+  std::size_t mask = 1;
+  for (size_t i = 0; i < x.size(); i++) {
+    if (u & mask)
+      x[i] = 1;
+    else
+      x[i] = 0;
+    mask <<= 1;
+  }
+}
+
+bit_vector_t
+hnco::bv_from_string(const std::string& str)
+{
+  bit_vector_t result;
+
+  result.reserve(str.size());
+  for (auto c : str)
+    if (c == '0')
+      result.push_back(0);
+    else
+      result.push_back(1);
+  assert(have_same_size(result, str));
+
+  return result;
+}
+
+bit_vector_t
+hnco::bv_from_stream(std::istream& stream)
+{
+  std::string line;
+
+  if (std::getline(stream, line))
+    return bv_from_string(line);
+  else
+    return bit_vector_t();
+}
---
+++ lib/hnco/log/logger.hh
@@ -0,0 +1,193 @@
+/* Copyright (C) 2016, 2017, 2018, 2019, 2020 Arnaud Berny
+
+   This file is part of HNCO.
+
+   HNCO is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   HNCO is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+   Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with HNCO. If not, see
+   <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef HNCO_BIT_VECTOR_H
+#define HNCO_BIT_VECTOR_H
+
+#include <algorithm>            // std::all_of, std::generate, std::fill
+#include <iostream>
+#include <numeric>              // std::accumulate
+#include <vector>
+
+#include "random.hh"
+
+extern "C" {
+  /// Dummy function to help autoconf
+  void check_libhnco(void);
+}
+
+/// top-level HNCO namespace
+namespace hnco {
+
+
+/** @name Types and functions related to bit
+ */
+///@{
+
+/** Bit.
+
+    A single bit is represented by a char.
+*/
+typedef char bit_t;
+
+/// Flip bit
+inline bit_t bit_flip(bit_t b) { return b ? 0 : 1; }
+
+/// Sample a random bit
+inline bit_t bit_random(double p) { return (random::Generator::uniform() < p) ? 1 : 0; }
+
+///@}
+
+
+/** @name Types and functions related to bit vectors
+
+    Output and input-output function parameters appear at the
+    beginning of the parameter list.
+
+    Output and input-output bit_vector_t parameters are passed by
+    reference and must have the right size for the considered
+    function.
+
+    Input bit_vector_t parameters are passed by const reference.
+*/
+///@{
+
+/// Bit vector
+typedef std::vector<bit_t> bit_vector_t;
+
+/// Display bit vector
+void bv_display(const bit_vector_t& v, std::ostream& stream);
+
+/// Check whether the bit vector is valid
+inline bool bv_is_valid(const bit_vector_t& x) { return std::all_of(x.begin(), x.end(), [](bit_t b){ return b == 0 || b == 1; }); }
+
+/// Check whether the bit vector is zero
+inline bool bv_is_zero(const bit_vector_t& x) { return std::all_of(x.begin(), x.end(), [](bit_t b){ return b == 0; }); }
+
+/// Hamming weight
+inline int bv_hamming_weight(const bit_vector_t& x) { return std::accumulate(x.begin(), x.end(), 0); }
+
+/// Hamming weight
+int bv_hamming_weight(const std::vector<bool>& x);
+
+/// Hamming distance between two bit vectors
+int bv_hamming_distance(const bit_vector_t& x, const bit_vector_t& y);
+
+/// Dot product
+bit_t bv_dot_product(const bit_vector_t& x, const bit_vector_t& y);
+
+/// Dot product
+bit_t bv_dot_product(const bit_vector_t& x, const std::vector<bool>& y);
+
+/// Clear bit vector
+inline void bv_clear(bit_vector_t& x) { std::fill(x.begin(), x.end(), 0); }
+
+/// Flip a single bit
+inline void bv_flip(bit_vector_t& x, int i) { x[i] = bit_flip(x[i]); }
+
+/// Flip many bits
+void bv_flip(bit_vector_t& x, const bit_vector_t& mask);
+
+/// Sample a random bit vector
+inline void bv_random(bit_vector_t& x) { std::generate(x.begin(), x.end(), []() { return random::Generator::bernoulli(); }); }
+
+/// Sample a random bit vector with given Hamming weight
+void bv_random(bit_vector_t& x, int k);
+
+/** Add two bit vectors.
+
+    Equivalent to dest = dest + src.
+
+    \param dest Destination bit vector
+    \param src Source bit vector
+
+    \warning Vectors must be of the same size.
+*/
+void bv_add(bit_vector_t& dest, const bit_vector_t& src);
+
+/** Add two bit vectors.
+
+    Equivalent to dest = x + y.
+
+    \param dest Destination bit vector
+    \param x First operand
+    \param y Second operand
+
+    \warning Vectors must be of the same size.
+*/
+void bv_add(bit_vector_t& dest, const bit_vector_t& x, const bit_vector_t& y);
+
+/** Convert a bit vector to a bool vector.
+
+    \warning Vectors must be of the same size.
+*/
+void bv_to_vector_bool(std::vector<bool>& y, const bit_vector_t& x);
+
+/** Convert a bool vector to a bit vector.
+
+    \warning Vectors must be of the same size.
+*/
+void bv_from_vector_bool(bit_vector_t& x, const std::vector<bool>& y);
+
+/** Convert a small bit vector to a size_t.
+
+    \param x Input bit vector
+
+    \return An unsigned integer representing x
+
+    \pre x.size() <= 8 * sizeof(std::size_t)
+*/
+std::size_t bv_to_size_type(const bit_vector_t& x);
+
+/** Convert a size_t to a small bit vector.
+
+    \param x Output bit vector
+    \param u Unsigned integer representing a bit vector
+
+    \pre x.size() <= 8 * sizeof(std::size_t)
+
+    \warning Depending on the size of the output bit vector, some bits
+    might be lost. The original bit vector can be reconstructed only
+    if it is small and the unsigned integer u is the result of
+    bv_to_size_type.
+*/
+void bv_from_size_type(bit_vector_t& x, std::size_t u);
+
+/** Read a bit vector from a string.
+
+    \param str Input string
+    \return A bit_vector_t
+*/
+bit_vector_t bv_from_string(const std::string& str);
+
+/** Read a bit vector from a stream.
+
+    \param stream Input stream
+    \return A bit_vector_t
+*/
+bit_vector_t bv_from_stream(std::istream& stream);
+
+///@}
+
+
+} // end of namespace hnco
+
+
+#endif
