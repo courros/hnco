232a2fef4eeb55429ed568e97fe95d8c33ebe48d
Arnaud Berny <hnco@courros.fr>
2021-03-04 23:32:09 +0100

    New file

---
+++ lib/hnco/functions/all.hh
@@ -0,0 +1,32 @@
+/* Copyright (C) 2016, 2017, 2018, 2019, 2020, 2021 Arnaud Berny
+
+   This file is part of HNCO.
+
+   HNCO is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   HNCO is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+   Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with HNCO. If not, see
+   <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef HNCO_FUNCTIONS_ALL_H
+#define HNCO_FUNCTIONS_ALL_H
+
+#include "collection/all.hh"
+#include "controllers/all.hh"
+#include "decorator.hh"
+#include "function.hh"
+#include "modifiers/all.hh"
+#include "representation/all.hh"
+#include "walsh-term.hh"
+
+#endif
