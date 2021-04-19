914c0ab17ab57425cf94b50a02184a28d5f3c013
Arnaud Berny <hnco@courros.fr>
2021-04-19 17:51:34 +0200

    Add python python

---
+++ bindings/python.cc
@@ -0,0 +1,12 @@
+#include <pybind11/pybind11.h>
+
+#include "hnco/functions/all.hh"
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(hnco, m) {
+  py::class_<OneMax>(m, "OneMax")
+    .def(py::init<int>())
+    .def("get_bv_size", &OneMax::get_bv_size)
+    .def("evaluate", &OneMax::evaluate);
+}
