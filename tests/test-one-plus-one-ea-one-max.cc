e1aebc8f6db9ddbc8ddf4f032bcfafaae66098dc
Arnaud Berny <hnco@courros.fr>
2021-06-28 09:36:15 +0200

    Catch TargetReached instead of MaximumReached

--- tests/test-one-plus-one-ea-one-max.cc
+++ tests/test-one-plus-one-ea-one-max.cc
@@ -54,7 +54,7 @@ int main(int argc, char *argv[])
     try {
       algorithm.maximize({&f2}); // finalize not necessary
     }
-    catch (const MaximumReached&) {
+    catch (const TargetReached&) {
       solution_t solution = f2.get_trigger();
       if (f1.evaluate(solution.first) != f1.get_maximum())
         return 1;
