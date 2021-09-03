008d073c567f554ff78960dcc3c04b8517e56316
Arnaud Berny <hnco@courros.fr>
2021-09-03 19:45:23 +0200

    Indent code

--- lib/hnco/neighborhoods/neighborhood-iterator.hh
+++ lib/hnco/neighborhoods/neighborhood-iterator.hh
@@ -26,80 +26,79 @@
 
 
 namespace hnco {
-
 namespace neighborhood {
 
 
-  /// %Neighborhood iterator
-  class NeighborhoodIterator:
+/// %Neighborhood iterator
+class NeighborhoodIterator:
     public Iterator {
 
-  public:
+public:
 
-    /** Constructor.
+  /** Constructor.
 
-        \param n Size of bit vectors
-    */
-    NeighborhoodIterator(int n):
-      Iterator(n) {}
-
-    /// Set origin
-    virtual void set_origin(const bit_vector_t& x);
-  };
-
-
-  /// Single bit flip neighborhood iterator
-  class SingleBitFlipIterator:
-    public NeighborhoodIterator {
-
-    /// Index of the last flipped bit
-    size_t _index;
-
-  public:
-
-    /** Constructor.
-
-        \param n Size of bit vectors
-    */
-    SingleBitFlipIterator(int n):
-      NeighborhoodIterator(n) {}
-
-    /// Has next bit vector
-    bool has_next() override;
-
-    /// Next bit vector
-    const bit_vector_t& next() override;
-
-  };
-
-
-  /** Hamming sphere neighborhood iterator.
+      \param n Size of bit vectors
   */
-  class HammingSphereIterator:
+  NeighborhoodIterator(int n):
+    Iterator(n) {}
+
+  /// Set origin
+  virtual void set_origin(const bit_vector_t& x);
+};
+
+
+/// Single bit flip neighborhood iterator
+class SingleBitFlipIterator:
     public NeighborhoodIterator {
 
-    /// Radius of the ball
-    int _radius;
+  /// Index of the last flipped bit
+  size_t _index;
 
-    /// Bit indexes
-    sparse_bit_vector_t _bit_indexes;
+public:
 
-  public:
+  /** Constructor.
 
-    /** Constructor.
+      \param n Size of bit vectors
+  */
+  SingleBitFlipIterator(int n):
+    NeighborhoodIterator(n) {}
 
-        \param n Size of bit vectors
-        \param r Radius of Hamming Ball
-    */
-    HammingSphereIterator(int n, int r);
+  /// Has next bit vector
+  bool has_next() override;
 
-    /// Has next bit vector
-    bool has_next() override;
+  /// Next bit vector
+  const bit_vector_t& next() override;
 
-    /// Next bit vector
-    const bit_vector_t& next() override;
+};
 
-  };
+
+/** Hamming sphere neighborhood iterator.
+ */
+class HammingSphereIterator:
+    public NeighborhoodIterator {
+
+  /// Radius of the ball
+  int _radius;
+
+  /// Bit indexes
+  sparse_bit_vector_t _bit_indexes;
+
+public:
+
+  /** Constructor.
+
+      \param n Size of bit vectors
+      \param r Radius of Hamming Ball
+  */
+  HammingSphereIterator(int n, int r);
+
+  /// Has next bit vector
+  bool has_next() override;
+
+  /// Next bit vector
+  const bit_vector_t& next() override;
+
+};
 
 
 } // end of namespace neighborhood
