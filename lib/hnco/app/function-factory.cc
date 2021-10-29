ab93d198767280a5fa6c1a37af1d19cc31571597
Arnaud Berny <hnco@courros.fr>
2021-10-29 21:38:14 +0200

    Set num_additional_bits to 2

--- lib/hnco/app/function-factory.cc
+++ lib/hnco/app/function-factory.cc
@@ -287,7 +287,7 @@ CommandLineFunctionFactory::make()
     int num_elements = instance->get_num_elements();
     return new PermutationFunctionAdapter<Tsp>
       (instance,
-       PermutationRepresentation(num_elements, num_elements + 2));
+       PermutationRepresentation(num_elements, 2));
   }
 
 #ifdef ENABLE_PLUGIN
