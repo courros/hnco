900e471c1576bb799f68df3a59c429af6cb744be
Arnaud Berny <hnco@courros.fr>
2021-10-30 19:20:09 +0200

    Implement MyFunctionFactory

--- examples/cli-universal-function.cc
+++ examples/cli-universal-function.cc
@@ -24,29 +24,56 @@
 #include "hnco/app/function-factory.hh"
 #include "hnco/app/application.hh"
 
+#include "hnco/functions/representations/all.hh"
+
 using namespace hnco::app;
 using namespace hnco::exception;
 using namespace hnco::function;
 using namespace hnco;
 
-// OneMax in dimension 8
-class MyFunction:
-  public Function {
+class MyFonction {
 public:
-  int get_bv_size() const { return 8; }
-  double evaluate(const bit_vector_t& x) {
-    int result = 0;
-    for (int i = 0; i < 8; i++)
-      if (x[i])
-        result++;
+  double evaluate(const bit_vector_t& boolean_vars,
+                  const std::vector<int>& integer_vars,
+                  const std::vector<double>& real_vars,
+                  const std::vector<std::complex<double>>& complex_vars,
+                  const std::vector<int>& categorical_vars,
+                  const std::vector<permutation_t> permutation_vars)
+  {
+    double result = 0;
     return result;
+  }
+  void display(std::ostream& stream) const {
+  }
+  void describe(const bit_vector_t& boolean_vars,
+                const std::vector<int>& integer_vars,
+                const std::vector<double>& real_vars,
+                const std::vector<std::complex<double>>& complex_vars,
+                const std::vector<int>& categorical_vars,
+                const std::vector<permutation_t> permutation_vars,
+                std::ostream& stream)
+  {
   }
 };
 
 class MyFunctionFactory:
   public FunctionFactory {
 public:
-  Function *make() { return new MyFunction(); }
+  Function *make() {
+    using namespace hnco::function::representation;
+    std::vector<DyadicIntegerRepresentation<int>> integer_reps(2);
+    std::vector<DyadicRealRepresentation<double>> real_reps(2);
+    std::vector<DyadicComplexRepresentation<double>> complex_reps(2);
+    std::vector<LinearCategoricalRepresentation> categorical_reps;
+    categorical_reps.push_back(LinearCategoricalRepresentation(4));
+    categorical_reps.push_back(LinearCategoricalRepresentation(3));
+    std::vector<PermutationRepresentation> permutation_reps;
+    permutation_reps.push_back(PermutationRepresentation(3, 2));
+    permutation_reps.push_back(PermutationRepresentation(5, 2));
+    return new UniversalFunctionAdapter(new MyFonction, 2, integer_reps, real_reps, complex_reps, categorical_reps, permutation_reps);
+    // Replace reps with {} is there is no corresponding variable as in :
+    // return new UniversalFunctionAdapter(new MyFonction, 2, integer_reps, real_reps, complex_reps, {}, permutation_reps);
+  }
 };
 
 int main(int argc, char *argv[])
