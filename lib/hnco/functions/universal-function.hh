5d925a16e633c8b1eaa2736eafb26a260c211b7e
Arnaud Berny <hnco@courros.fr>
2021-11-02 10:24:59 +0100

    Add file

---
+++ lib/hnco/functions/universal-function.hh
@@ -0,0 +1,67 @@
+/* Copyright (C) 2016, 2017, 2018, 2019, 2020, 2021 Arnaud Berny
+
+   This file is part of HNCO.
+
+   HNCO is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   HNCO is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+   Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with HNCO. If not, see
+   <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef HNCO_FUNCTIONS_UNIVERSAL_FUNCTION_H
+#define HNCO_FUNCTIONS_UNIVERSAL_FUNCTION_H
+
+#include <iostream>
+
+#include "hnco/bit-vector.hh"
+#include "hnco/functions/representations/all.hh"
+
+
+namespace hnco {
+namespace function {
+
+/** Universal function.
+
+ */
+class UniversalFunction {
+public:
+
+  /// Evaluate
+  virtual double evaluate(const bit_vector_t& boolean_vars,
+                          const std::vector<int>& integer_vars,
+                          const std::vector<double>& real_vars,
+                          const std::vector<std::complex<double>>& complex_vars,
+                          const std::vector<int>& categorical_vars,
+                          const std::vector<permutation_t> permutation_vars) = 0;
+
+  /// Display the function
+  virtual void display(std::ostream& stream) const {}
+
+  /// Describe
+  virtual void describe(const bit_vector_t& boolean_vars,
+                        const std::vector<int>& integer_vars,
+                        const std::vector<double>& real_vars,
+                        const std::vector<std::complex<double>>& complex_vars,
+                        const std::vector<int>& categorical_vars,
+                        const std::vector<permutation_t> permutation_vars,
+                        std::ostream& stream)
+  {}
+
+};
+
+
+} // end of namespace function
+} // end of namespace hnco
+
+
+#endif
