de602ad5c6f81e4067438a6f1d67e85f291af0b6
Arnaud Berny <hnco@courros.fr>
2021-12-25 16:17:38 +0100

    Add clip_value and is_in_interval

--- lib/hnco/util.hh
+++ lib/hnco/util.hh
@@ -57,6 +57,28 @@ inline bool is_in_range(int i, int n) { return is_in_range(i, 0, n); }
 
 ///@}
 
+/** @name Intervals
+ */
+///@{
+
+/// Check whether a float value belongs to a given interval
+inline bool is_in_interval(double x, double a, double b) { return x >= a && x <= b; }
+
+/// Clip value between two bounds
+template<typename T>
+T clip_value(T x, T low, T high)
+{
+  assert(low < high);
+
+  if (x > high)
+    return high;
+  else if (x < low)
+    return low;
+  else
+    return x;
+}
+
+///@}
 
 /// Check whether two containers have the same size
 template<class A, class B>
