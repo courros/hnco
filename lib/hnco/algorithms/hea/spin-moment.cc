4feb32f6b434ed750488406d9e930905fb1cd933
Arnaud Berny <hnco@courros.fr>
2021-12-29 15:38:59 +0100

    Add SymmetricWalshMoment2

--- lib/hnco/algorithms/hea/spin-moment.cc
+++ lib/hnco/algorithms/hea/spin-moment.cc
@@ -21,6 +21,7 @@
 #include <assert.h>
 #include <math.h>		// std::sqrt
 
+#include "hnco/algorithms/matrix.hh" // hnco::algorithm::matrix_is_symmetric
 #include "hnco/util.hh"
 
 #include "spin-moment.hh"
@@ -48,8 +49,6 @@ LowerTriangularWalshMoment2::display(std::ostream& stream)
       stream << second_moment[j][i] << " ";
     stream << std::endl;
   }
-  stream << std::endl;
-  stream << std::endl;
 }
 
 void
@@ -219,6 +218,225 @@ LowerTriangularWalshMoment2::norm_infinite() const
 
 double
 LowerTriangularWalshMoment2::distance(const LowerTriangularWalshMoment2& wm) const
+{
+  assert(have_same_size(wm.first_moment, first_moment));
+
+  double result = 0;
+  for (size_t i = 0; i < first_moment.size(); i++) {
+    result += square(first_moment[i] - wm.first_moment[i]);
+    const std::vector<double>& row = second_moment[i];
+    const std::vector<double>& row2 = wm.second_moment[i];
+    for (size_t j = 0; j < i; j++)
+      result += square(row[j] - row2[j]);
+  }
+  return std::sqrt(result);
+}
+
+SymmetricWalshMoment2::SymmetricWalshMoment2(int n)
+{
+  assert(n > 0);
+
+  first_moment.resize(n);
+  second_moment.resize(n);
+  for (auto& row : second_moment)
+    row.resize(n);
+}
+
+void
+SymmetricWalshMoment2::display(std::ostream& stream)
+{
+  const size_t dimension = first_moment.size();
+  for (size_t i = 0; i < dimension; i++) {
+    const std::vector<double>& row = second_moment[i];
+    for (size_t j = 0; j < dimension; j++) {
+      if (i == j)
+        stream << first_moment[i] << " ";
+      else
+        stream << row[j] << " ";
+    }
+    stream << std::endl;
+  }
+}
+
+void
+SymmetricWalshMoment2::init()
+{
+  std::fill(first_moment.begin(), first_moment.end(), 0);
+  for (auto& row : second_moment)
+    std::fill(row.begin(), row.end(), 0);
+}
+
+void
+SymmetricWalshMoment2::add(const bit_vector_t& bv)
+{
+  assert(have_same_size(bv, first_moment));
+
+  for (size_t i = 0; i < bv.size(); i++) {
+    std::vector<double>& row = second_moment[i];
+    if (bv[i]) {
+      first_moment[i]--;
+      for (size_t j = 0; j < i; j++)
+        if (bv[j])
+          row[j]++;
+        else
+          row[j]--;
+    } else {
+      first_moment[i]++;
+      for (size_t j = 0; j < i; j++)
+        if (bv[j])
+          row[j]--;
+        else
+          row[j]++;
+    }
+  }
+}
+
+void
+SymmetricWalshMoment2::average(int count)
+{
+  for (size_t i = 0; i < first_moment.size(); i++) {
+    first_moment[i] /= count;
+    assert(is_in_interval(first_moment[i], -1, 1));
+
+    std::vector<double>& row = second_moment[i];
+    for (size_t j = 0; j < i; j++) {
+      row[j] /= count;
+      assert(is_in_interval(row[j], -1, 1));
+
+      second_moment[j][i] = row[j];
+    }
+  }
+  assert(matrix_is_symmetric(second_moment));
+}
+
+void
+SymmetricWalshMoment2::update(const SymmetricWalshMoment2& wm, double rate)
+{
+  assert(have_same_size(wm.first_moment, first_moment));
+
+  for (size_t i = 0; i < first_moment.size(); i++) {
+    first_moment[i] += rate * (wm.first_moment[i] - first_moment[i]);
+    assert(is_in_interval(first_moment[i], -1, 1));
+
+    std::vector<double>& row = second_moment[i];
+    const std::vector<double>& row2 = wm.second_moment[i];
+    for (size_t j = 0; j < i; j++) {
+      row[j] += rate * (row2[j] - row[j]);
+      assert(is_in_interval(row[j], -1, 1));
+
+      second_moment[j][i] = row[j];
+    }
+  }
+  assert(matrix_is_symmetric(second_moment));
+}
+
+void
+SymmetricWalshMoment2::update(const SymmetricWalshMoment2& wm1,
+                                    const SymmetricWalshMoment2& wm2,
+                                    double rate)
+{
+  assert(have_same_size(wm1.first_moment, first_moment));
+  assert(have_same_size(wm2.first_moment, first_moment));
+
+  for (size_t i = 0; i < first_moment.size(); i++) {
+    first_moment[i] += rate * (wm1.first_moment[i] - wm2.first_moment[i]);
+
+    std::vector<double>& row = second_moment[i];
+    const std::vector<double>& row1 = wm1.second_moment[i];
+    const std::vector<double>& row2 = wm2.second_moment[i];
+    for (size_t j = 0; j < i; j++) {
+      row[j] += rate * (row1[j] - row2[j]);
+
+      second_moment[j][i] = row[j];
+    }
+  }
+}
+
+void
+SymmetricWalshMoment2::scaled_difference(double lambda,
+                                              const SymmetricWalshMoment2& wm1,
+                                              const SymmetricWalshMoment2& wm2)
+{
+  assert(have_same_size(wm1.first_moment, first_moment));
+  assert(have_same_size(wm2.first_moment, first_moment));
+
+  for (size_t i = 0; i < first_moment.size(); i++) {
+    first_moment[i] = lambda * wm1.first_moment[i] - wm2.first_moment[i];
+    std::vector<double>& row = second_moment[i];
+    const std::vector<double>& row1 = wm1.second_moment[i];
+    const std::vector<double>& row2 = wm2.second_moment[i];
+    for (size_t j = 0; j < i; j++) {
+      row[j] = lambda * row1[j] - row2[j];
+
+      second_moment[j][i] = row[j];
+    }
+  }
+}
+
+void
+SymmetricWalshMoment2::bound(double margin)
+{
+  assert(is_in_interval(margin, 0, 1));
+
+  const double high = 1 - margin;
+  const double low = margin - 1;
+  assert(low < high);
+
+  for (size_t i = 0; i < first_moment.size(); i++) {
+    first_moment[i] = clip_value(first_moment[i], low, high);
+    assert(is_in_interval(first_moment[i], -1, 1));
+
+    std::vector<double>& row = second_moment[i];
+    for (size_t j = 0; j < i; j++) {
+      row[j] = clip_value(row[j], low, high);
+      assert(is_in_interval(row[j], -1, 1));
+
+      second_moment[j][i] = row[j];
+    }
+  }
+}
+
+double
+SymmetricWalshMoment2::norm_1() const
+{
+  double result = 0;
+  for (size_t i = 0; i < first_moment.size(); i++) {
+    result += std::fabs(first_moment[i]);
+    const std::vector<double>& row = second_moment[i];
+    for (size_t j = 0; j < i; j++)
+      result += std::fabs(row[j]);
+  }
+  return result;
+}
+
+double
+SymmetricWalshMoment2::norm_2() const
+{
+  double result = 0;
+  for (size_t i = 0; i < first_moment.size(); i++) {
+    result += square(first_moment[i]);
+    const std::vector<double>& row = second_moment[i];
+    for (size_t j = 0; j < i; j++)
+      result += square(row[j]);
+  }
+  return std::sqrt(result);
+}
+
+double
+SymmetricWalshMoment2::norm_infinite() const
+{
+  double result = 0;
+  for (size_t i = 0; i < first_moment.size(); i++) {
+    result = std::max(result, first_moment[i]);
+    const std::vector<double>& row = second_moment[i];
+    for (size_t j = 0; j < i; j++)
+      result = std::max(result, row[j]);
+  }
+  return result;
+}
+
+double
+SymmetricWalshMoment2::distance(const SymmetricWalshMoment2& wm) const
 {
   assert(have_same_size(wm.first_moment, first_moment));
 
