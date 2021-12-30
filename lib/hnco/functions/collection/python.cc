9e85e7b23170c3f4be89b64c8f5a89c0922b8018
Arnaud Berny <hnco@courros.fr>
2021-12-30 11:25:18 +0100

    Add files python.hh and python.cc

---
+++ lib/hnco/functions/collection/python.cc
@@ -0,0 +1,64 @@
+/* Copyright (C) 2016, 2017, 2018, 2019, 2020, 2021 Arnaud Berny
+
+   This file is part of HNCO.
+
+   HNCO is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   HNCO is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+   Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with HNCO. If not, see
+   <http://www.gnu.org/licenses/>.
+
+*/
+
+#include <assert.h>
+#include <dlfcn.h>              // dlopen, dlsym, dlclose
+
+#include <sstream>              // std::ostringstream
+
+#include "hnco/exception.hh"
+
+#include "plugin.hh"
+
+
+using namespace hnco::function;
+using namespace hnco::exception;
+
+
+FunctionPlugin::FunctionPlugin(int bv_size, std::string path, std::string name):
+  _bv_size(bv_size)
+{
+  _handle = dlopen(path.c_str(), RTLD_LAZY);
+  if (!_handle) {
+    std::ostringstream stream;
+    stream << "FunctionPlugin::FunctionPlugin: "
+           << std::string(dlerror());
+    throw std::runtime_error(stream.str());
+  }
+  dlerror();
+  _extern_function = (extern_function_t) dlsym(_handle, name.c_str());
+  char *error = dlerror();
+  if (error != NULL) {
+    std::ostringstream stream;
+    stream << "FunctionPlugin::FunctionPlugin: " << std::string(error);
+    throw std::runtime_error(stream.str());
+  }
+}
+
+FunctionPlugin::~FunctionPlugin()
+{
+  dlclose(_handle);
+}
+
+double FunctionPlugin::evaluate(const bit_vector_t& x)
+{
+  assert(int(x.size()) == _bv_size);
+  return (*_extern_function)(x.data(), _bv_size);
+}
---
+++ lib/hnco/functions/collection/python.hh
@@ -0,0 +1,75 @@
+/* Copyright (C) 2016, 2017, 2018, 2019, 2020, 2021 Arnaud Berny
+
+   This file is part of HNCO.
+
+   HNCO is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   HNCO is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+   Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with HNCO. If not, see
+   <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef HNCO_FUNCTIONS_COLLECTION_PLUGIN_H
+#define HNCO_FUNCTIONS_COLLECTION_PLUGIN_H
+
+#include <string>
+
+#include "hnco/functions/function.hh"
+
+
+namespace hnco {
+namespace function {
+
+
+/// %Function plugin
+class FunctionPlugin:
+    public Function {
+
+  /// Bit vector size
+  int _bv_size;
+
+  /// Handle returned by dlopen
+  void *_handle;
+
+  /// Type of an extern function
+  typedef double (*extern_function_t)(const bit_t *, size_t);
+
+  /// Extern function
+  extern_function_t _extern_function;
+
+public:
+
+  /** Constructor.
+
+      \param bv_size Size of bit vectors
+      \param path Path to a shared library
+      \param name Name of a function of the shared library
+  */
+  FunctionPlugin(int bv_size, std::string path, std::string name);
+
+  /// Destructor
+  ~FunctionPlugin();
+
+  /// Get bit vector size
+  int get_bv_size() const { return _bv_size; }
+
+  /// Evaluate a bit vector
+  double evaluate(const bit_vector_t&);
+
+};
+
+
+} // end of namespace function
+} // end of namespace hnco
+
+
+#endif
