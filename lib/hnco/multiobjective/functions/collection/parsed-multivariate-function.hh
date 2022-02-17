ae38189052486fb1ebbc7d746878dc8f5047ef85
Arnaud Berny <hnco@courros.fr>
2022-02-17 16:04:47 +0100

    Add files for multivariate functions

---
+++ lib/hnco/multiobjective/functions/collection/parsed-multivariate-function.hh
@@ -0,0 +1,111 @@
+/* Copyright (C) 2016, 2017, 2018, 2019, 2020, 2021, 2022 Arnaud Berny
+
+   This file is part of HNCO.
+
+   HNCO is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   HNCO is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+   Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with HNCO. If not, see
+   <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef HNCO_FUNCTIONS_COLLECTION_PARSED_MULTIVARIATE_FUNCTION_H
+#define HNCO_FUNCTIONS_COLLECTION_PARSED_MULTIVARIATE_FUNCTION_H
+
+#include <assert.h>
+
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include "hnco/util.hh"
+
+#include "fparser/fparser.hh"
+
+namespace hnco {
+namespace function {
+
+/** Parsed multivariate function.
+
+    Uses the C++ library "Function Parser" (fparser):
+
+    http://warp.povusers.org/FunctionParser/fparser.html
+
+    \warning The function string syntax depends on the chosen parser.
+
+*/
+template<class Parser>
+class ParsedMultivariateFunction {
+
+  /// Function parser
+  Parser _fparser;
+
+  /// Variable names
+  std::vector<std::string> _variable_names;
+
+  /// Expression
+  std::string _expression;
+
+public:
+
+  /// Domain type
+  using domain_type = typename Parser::value_type;
+
+  /// Codomain type
+  using codomain_type = typename Parser::value_type;
+
+  /** Constructor.
+
+      \param expression Expression to parse
+  */
+  ParsedMultivariateFunction(std::string expression)
+    : _expression(expression)
+  {
+    int position = _fparser.ParseAndDeduceVariables(_expression, _variable_names);
+    if (position != -1) {
+      std::ostringstream stream;
+      stream
+        << "ParsedMultivariateFunction::ParsedMultivariateFunction: " << _fparser.ErrorMsg()
+        << " at position: " << position
+        << " in _expression: " << _expression;
+      throw std::runtime_error(stream.str());
+    }
+    _fparser.Optimize();
+  }
+
+  /// Display the problem
+  void display(std::ostream& stream) const {
+    stream << "Expression: f(" << join(begin(_variable_names), end(_variable_names), ", ") << ") = " << _expression << std::endl;
+  }
+
+  /// Evaluate
+  codomain_type evaluate(const std::vector<domain_type>& x) { return _fparser.Eval(x.data()); }
+
+  /// Describe a solution
+  void describe(const std::vector<domain_type>& x, std::ostream& stream) {
+    assert(x.size() == _variable_names.size());
+    for (std::size_t i = 0; i < x.size(); i++)
+      stream << _variable_names[i] << " = " << x[i] << std::endl;
+  }
+
+  /// Get the number of variables
+  int get_num_variables() { return _variable_names.size(); }
+
+};
+
+
+} // end of namespace function
+} // end of namespace hnco
+
+
+#endif
---
+++ lib/hnco/multiobjective/functions/multivariate-function-adapter.hh
@@ -0,0 +1,169 @@
+/* Copyright (C) 2016, 2017, 2018, 2019, 2020, 2021, 2022 Arnaud Berny
+
+   This file is part of HNCO.
+
+   HNCO is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   HNCO is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+   Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with HNCO. If not, see
+   <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef HNCO_FUNCTIONS_MULTIVARIATE_FUNCTION_ADAPTER_H
+#define HNCO_FUNCTIONS_MULTIVARIATE_FUNCTION_ADAPTER_H
+
+#include <assert.h>
+
+#include <type_traits>          // std::is_same
+
+#include "hnco/representations/all.hh"
+
+#include "function.hh"
+
+
+namespace hnco {
+namespace multiobjective {
+namespace function {
+
+
+/** Multivariate function adapter.
+
+    The purpose of this class is to build a regular hnco function from
+    an arbitrary multivariate function. This is achieved using a
+    composition:
+    - Representations (Rep): hypercube -> domain
+    - Multivariate function (Fn): product of domains -> codomain
+    - Converter (Conv): codomain -> double
+*/
+template<class Fn, class Rep, class Conv>
+class MultivariateFunctionAdapter: public Function {
+  static_assert(std::is_same<
+                typename Fn::domain_type,
+                typename Rep::domain_type
+                >::value,
+                "multiobjective::function::MultivariateFunctionAdapter: domain types do not match");
+  static_assert(std::is_same<
+                typename Fn::codomain_type,
+                typename Conv::codomain_type
+                >::value,
+                "multiobjective::function::MultivariateFunctionAdapter: codomain types do not match");
+
+  /// Multivariate function
+  Fn *_function;
+
+  /// Representations
+  std::vector<Rep> _representations;
+
+  /// Variables
+  std::vector<typename Rep::domain_type> _variables;
+
+  /// Converter from codomain to double
+  Conv _converter;
+
+  /// Codomain value
+  std::vector<typename Fn::codomain_type> _codomain_value;
+
+  /// Unpack a bit vector into variables
+  void unpack(const bit_vector_t& bv) {
+    int start = 0;
+    for (size_t i = 0; i < _variables.size(); i++) {
+      _variables[i] = _representations[i].unpack(bv, start);
+      start += _representations[i].size();
+    }
+  }
+
+public:
+
+  /** Constructor.
+
+      \param fn Multivariate function
+      \param reps Representations
+  */
+  MultivariateFunctionAdapter(Fn *fn, std::vector<Rep> reps)
+    : _function(fn)
+    , _representations(reps)
+    , _variables(reps.size())
+  {
+    assert(fn);
+    assert(fn->get_num_variables() == int(reps.size()));
+
+    _codomain_value.resize(_function->get_output_size());
+  }
+
+  /** @name Information about the function
+   */
+  ///@{
+
+  /// Get bit vector size
+  int get_bv_size() const override {
+    int result = 0;
+    for (const auto& rep : _representations)
+      result += rep.size();
+    return result;
+  }
+
+  /// Get output size (number of objectives)
+  int get_output_size() const override {
+    return _function->get_output_size();
+  }
+
+  ///@}
+
+
+  /** @name Evaluation
+   */
+  ///@{
+
+  /// Evaluate
+  void evaluate(const bit_vector_t& bv, value_t& value) override {
+    assert(get_bv_size() == int(bv.size()));
+    assert(get_output_size() == int(value.size()));
+
+    unpack(bv);
+    _function->evaluate(_variables, _codomain_value);
+    for (int i = 0; i < get_output_size(); i++)
+      value[i] = _converter(_codomain_value[i]);
+  }
+
+  ///@}
+
+
+  /** @name Display
+   */
+  ///@{
+
+  /// Display
+  void display(std::ostream& stream) const override {
+    _function->display(stream);
+    stream << _function->get_num_variables() << " variables:" << std::endl;
+    for (const auto& rep : _representations)
+      rep.display(stream);
+    stream << get_bv_size() << " bits" << std::endl;
+  }
+
+  /// Describe a bit vector
+  void describe(const bit_vector_t& bv, std::ostream& stream) override {
+    unpack(bv);
+    _function->describe(_variables, stream);
+  }
+
+  ///@}
+
+};
+
+
+} // end of namespace function
+} // end of namespace multiobjective
+} // end of namespace hnco
+
+
+#endif
