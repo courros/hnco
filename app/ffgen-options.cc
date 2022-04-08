a78abeda329f0e88d8745977e29d23a6f5fbf61b
Arnaud Berny <hnco@courros.fr>
2022-04-08 21:42:07 +0200

    Update version number
    From 0.19 to 0.20

--- app/ffgen-options.cc
+++ app/ffgen-options.cc
@@ -8,7 +8,7 @@ using namespace hnco::app;
 
 FfgenOptions::FfgenOptions(int argc, char *argv[]):
   _exec_name(argv[0]),
-  _version("0.19"),
+  _version("0.20"),
   _bv_size(100),
   _opt_bv_size(false),
   _coupling_constant(1),
