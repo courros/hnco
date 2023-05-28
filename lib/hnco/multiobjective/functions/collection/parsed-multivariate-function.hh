7eebf85816c813a2ae926cc0a9f7cbf0b647c44d
Arnaud Berny <hnco@courros.fr>
2023-05-28 08:48:10 +0200

    Add multiobjective::ParsedMultivariateFunction::get_variable_names()

--- lib/hnco/multiobjective/functions/collection/parsed-multivariate-function.hh
+++ lib/hnco/multiobjective/functions/collection/parsed-multivariate-function.hh
@@ -29,6 +29,7 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
+#include <algorithm>            // std::sort
 
 #include "hnco/util.hh"         // hnco::join
 
@@ -40,15 +41,15 @@ namespace multiobjective {
 namespace function {
 
 
-/** Parsed multivariate function.
-
-    Uses the C++ library "Function Parser" (fparser):
-
-    http://warp.povusers.org/FunctionParser/fparser.html
-
-    \warning The function string syntax depends on the chosen parser.
-
-*/
+/**
+ * Parsed multivariate function.
+ *
+ * Uses the C++ library "Function Parser" (fparser):
+ *
+ * http://warp.povusers.org/FunctionParser/fparser.html
+ *
+ * \warning The function string syntax depends on the chosen parser.
+ */
 template<class Parser>
 class ParsedMultivariateFunction {
 
@@ -58,7 +59,7 @@ public:
   using domain_type = typename Parser::value_type;
 
   /// Codomain type
-  using codomain_type = typename Parser::value_type;
+  using codomain_type = domain_type;
 
 private:
 
@@ -68,40 +69,48 @@ private:
   /// %Function parsers
   std::vector<Parser> _parsers;
 
-  /** Variable names.
-
-      Indexed by parser index.
-  */
+  /**
+   * Names.
+   *
+   * Indexed by parser then variable. Then, _names[i][j] is the name
+   * of the jth variable of the ith parser.
+   */
   std::vector<std::vector<std::string>> _names;
 
-  /** Variables.
-
-      Indexed by parser index.
-  */
+  /**
+   * Variables.
+   *
+   * Indexed by parser then variable. Then, _variables[i][j] is the
+   * value of the jth variable of the ith parser.
+   */
   std::vector<std::vector<domain_type>> _variables;
 
-  /** Lookup tables.
+  /**
+   * Indices.
+   *
+   * Indexed by parser then variable. Then, _indices[i][j] is the index
+   * in the variable to evaluate (x) of the jth variable of the ith
+   * parser.
+   */
+  std::vector<std::vector<int>> _indices;
 
-      Indexed by parser index.
-  */
-  std::vector<std::vector<int>> _lookup_tables;
-
-  /** Index of variable.
-
-      In the vector of variables to be evaluated.
-  */
-  std::unordered_map<std::string, int> _index_of;
+  /**
+   * Ordered variable names.
+   *
+   * As expected by evaluate().
+   */
+  std::vector<std::string> _ordered_names;
 
 public:
 
-  /** Constructor.
-
-      An expression is a list of sub expressions separated by double
-      colons (::). Each sub expression defines a multivariate
-      function.
-
-      \param expression Expression to parse
-  */
+  /**
+   * Constructor.
+   *
+   * An expression is a list of sub expressions separated by double
+   * colons (::). Each sub expression defines a multivariate function.
+   *
+   * \param expression Expression to parse
+   */
   ParsedMultivariateFunction(std::string expression)
   {
     // Split expression into sub expressions
@@ -133,31 +142,35 @@ public:
       _variables[i].resize(_names[i].size());
     }
 
-    // All names
-    std::unordered_set<std::string> all_names;
+    // Collect all names into a set
+    std::unordered_set<std::string> name_set;
     for (size_t i = 0; i < _names.size(); i++) {
       for (const auto& name : _names[i])
-        all_names.insert(name);
+        name_set.insert(name);
     }
 
+    _ordered_names.resize(name_set.size());
+    std::unordered_map<std::string, int> index_of;
     int index = 0;
-    for (const auto& name : all_names)
-      _index_of[name] = index++;
+    for (const auto& name : name_set) {
+      assert(is_in_range(index, _ordered_names.size()));
+      _ordered_names[index] = name;
+      index_of[name] = index++;
+    }
 
-    // build lookup tables
-    _lookup_tables.resize(_expressions.size());
-    for (size_t i = 0; i < _lookup_tables.size(); i++) {
-      _lookup_tables[i].resize(_names[i].size());
+    // Compute indices
+    _indices.resize(_expressions.size());
+    for (size_t i = 0; i < _indices.size(); i++) {
+      _indices[i].resize(_names[i].size());
       for (size_t j = 0; j < _names[i].size(); j++) {
-        assert(_index_of.find(_names[i][j]) != _index_of.end());
-
-        _lookup_tables[i][j] = _index_of[_names[i][j]];
+        assert(index_of.find(_names[i][j]) != index_of.end());
+        _indices[i][j] = index_of[_names[i][j]];
       }
     }
   }
 
   /// Get the number of variables
-  int get_num_variables() const { return _index_of.size(); }
+  int get_num_variables() const { return _ordered_names.size(); }
 
   /// Get output size (number of objectives)
   int get_output_size() const { return _parsers.size(); }
@@ -169,7 +182,7 @@ public:
 
     for (size_t i = 0; i < values.size(); i++) {
       auto& vars = _variables[i];
-      auto& lut = _lookup_tables[i];
+      auto& lut = _indices[i];
       for (size_t j = 0; j < vars.size(); j++) {
         assert(is_in_range(lut[j], x.size()));
         vars[j] = x[lut[j]];
@@ -181,24 +194,24 @@ public:
   /// Display the problem
   void display(std::ostream& stream) const {
     stream << "ParsedMultivariateFunction:" << std::endl;
-    std::vector<std::string> vars(_index_of.size());
-    for (const auto& kv : _index_of)
-      vars[kv.second] = kv.first;
-    stream << "Variables: " << hnco::join(vars.begin(), vars.end(), ", ") << std::endl;
+    auto sorted_names = _ordered_names;
+    std::sort(begin(sorted_names), end(sorted_names));
+    stream << "Variables: " << hnco::join(sorted_names.begin(), sorted_names.end(), ", ") << std::endl;
     stream << "Objectives:" << std::endl;
     for (size_t i = 0; i < _parsers.size(); i++)
-      stream << hnco::join(_names[i].begin(), _names[i].end(), ", ") << " -> " << _expressions[i] << std::endl;
+      stream << i << ": " << hnco::join(_names[i].begin(), _names[i].end(), ", ") << " -> " << _expressions[i] << std::endl;
   }
 
   /// Describe a solution
   void describe(const std::vector<domain_type>& x, std::ostream& stream) {
     assert(int(x.size()) == get_num_variables());
 
-    for (const auto& kv : _index_of) {
-      assert(is_in_range(kv.second, x.size()));
-      stream << kv.first << " = " << x[kv.second] << std::endl;
-    }
+    for (size_t i = 0; i < _ordered_names.size(); i++)
+      stream << _ordered_names[i] << " = " << x[i] << std::endl;
   }
+
+  /// Get variable names
+  const std::vector<std::string>& get_variable_names() { return _ordered_names; }
 
 };
 
