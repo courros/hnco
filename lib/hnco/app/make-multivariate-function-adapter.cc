652ad93199305aad50c5fb2845538093693f77a6
Arnaud Berny <hnco@courros.fr>
2023-06-10 20:49:08 +0200

    Add parse_size_declaration and parse_sizes

--- lib/hnco/app/make-multivariate-function-adapter.cc
+++ lib/hnco/app/make-multivariate-function-adapter.cc
@@ -22,3 +22,62 @@
 
 using namespace hnco::app;
 using namespace hnco;
+
+std::optional<std::pair<std::string, int>> parse_size_declaration(std::string expression)
+{
+  const std::string delimiter = ":";
+
+  auto start = 0U;
+  auto stop = expression.find(delimiter);
+  if (stop == std::string::npos) {
+    std::cerr << "parse_size_declaration: Missing colon" << std::endl;
+    return {};
+  }
+
+  auto before = expression.substr(start, stop - start);
+  std::istringstream stream(before);
+  std::string name;
+  stream >> name;
+  if (stream.fail()) {
+    std::cerr << "parse_size_declaration: Expected variable name before colon" << std::endl;
+    return {};
+  }
+
+  start = stop + delimiter.length();
+  auto after = expression.substr(start);
+  stream.str(after);
+  int size;
+  stream >> size;
+  if (stream.fail()) {
+    std::cerr << "parse_size_declaration: Expected size" << std::endl;
+    return {};
+  }
+
+  return std::make_pair(name, size);
+
+}
+
+std::unordered_map<std::string, int> parse_sizes(std::string expression)
+{
+  const std::string delimiter = ";";
+
+  std::unordered_map<std::string, int> sizes;
+  auto start = 0U;
+  auto stop = expression.find(delimiter);
+
+  if (stop == std::string::npos) {
+    auto opt = parse_size_declaration(expression);
+    if (opt)
+      sizes.insert(opt.value());
+    return sizes;
+  }
+
+  while (stop != std::string::npos) {
+    auto opt = parse_size_declaration(expression.substr(start, stop - start));
+    if (opt)
+      sizes.insert(opt.value());
+    start = stop + delimiter.length();
+    stop = expression.find(delimiter, start);
+  }
+  return sizes;
+}
