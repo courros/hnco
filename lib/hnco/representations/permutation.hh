6cf33b8ce8b6bd7dba4e36f24ae0f6575ba89a86
Arnaud Berny <hnco@courros.fr>
2023-06-20 15:04:21 +0200

    Clean code

--- lib/hnco/representations/permutation.hh
+++ lib/hnco/representations/permutation.hh
@@ -53,39 +53,40 @@ private:
   /// Elements
   std::vector<Element> _elements;
 
-  /// Number of bits per element
-  int _num_bits;
+  /// Element size in bits
+  int _element_size;
 
-  /// Representation size
-  int _representation_size;
+  /// Size in bits
+  int _size;
 
 public:
 
-  /** Constructor.
-
-      Each element is represented by an integer encoded using
-      std::ceil(std::log(num_elements) / std::log(2)) +
-      num_additional_bits.
-
-      \param num_elements Number of elements
-      \param num_additional_bits Number of additional bits per element
-  */
+  /**
+   * Constructor.
+   *
+   * Each element is represented by an integer encoded using
+   * std::ceil(std::log(num_elements) / std::log(2)) +
+   * num_additional_bits.
+   *
+   * \param num_elements Number of elements
+   * \param num_additional_bits Number of additional bits per element
+   */
   PermutationRepresentation(int num_elements, int num_additional_bits)
     : _elements(num_elements)
   {
     assert(num_elements > 0);
     assert(num_additional_bits > 0);
 
-    int num_bits_min = std::ceil(std::log(num_elements) / std::log(2));
-    _num_bits = num_bits_min + num_additional_bits;
-    _representation_size = _elements.size() * _num_bits;
+    int exact_element_size = std::ceil(std::log(num_elements) / std::log(2));
+    _element_size = exact_element_size + num_additional_bits;
+    _size = _elements.size() * _element_size;
   }
 
   /// Get number of elements
   int get_num_elements() const { return _elements.size(); }
 
   /// Size of the representation
-  int size() const { return _representation_size; }
+  int size() const { return _size; }
 
   /// Unpack bit vector into a permutation
   void unpack(const bit_vector_t& bv, int start, hnco::permutation_t& permutation) {
@@ -94,11 +95,12 @@ public:
 
     for (size_t i = 0; i < _elements.size(); i++) {
       _elements[i].index = i;
-      _elements[i].value = bv_to_size_type(bv, start, start + _num_bits);
-      start += _num_bits;
+      _elements[i].value = bv_to_size_type(bv, start, start + _element_size);
+      start += _element_size;
     }
 
-    std::sort(_elements.begin(), _elements.end(), [](Element& a, Element& b){ return a.value < b.value; });
+    std::sort(_elements.begin(), _elements.end(),
+              [](Element& a, Element& b){ return a.value < b.value; });
     for (size_t i = 0; i < permutation.size(); i++)
       permutation[i] = _elements[i].index;
     assert(hnco::perm_is_valid(permutation));
@@ -106,7 +108,9 @@ public:
 
   /// Display
   void display(std::ostream& stream) const {
-    stream << "PermutationRepresentation (" << _elements.size() << " elements) (" << _elements.size() << " * " << _num_bits << " bits)";
+    stream << "PermutationRepresentation ("
+           << _elements.size() << " elements) ("
+           << _elements.size() << " * " << _element_size << " bits)";
   }
 
 };
