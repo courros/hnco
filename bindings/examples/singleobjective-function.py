b4fe4757043cb3752432783e529d5b8fabec8e9a
Arnaud Berny <hnco@courros.fr>
2023-10-17 17:57:07 +0200

    Update example

--- bindings/examples/singleobjective-function.py
+++ bindings/examples/singleobjective-function.py
@@ -1,5 +1,9 @@
 import hnco
+import hnco.algorithm as algo
 import hnco.function as fn
+import hnco.random
+
+hnco.random.Generator.set_seed()
 
 class MyFunction(fn.Function):
     def __init__(self, n):
@@ -12,10 +16,11 @@ class MyFunction(fn.Function):
     def get_bv_size(self):
         return self.bv_size
 
-    def has_known_maximum(self):
-        return True
-
-    def get_maximum(self):
-        return self.bv_size
-
 f = MyFunction(100)
+
+a = algo.OnePlusOneEa(f.get_bv_size())
+a.set_num_iterations(1000)
+a.maximize([f])
+a.finalize()                    # Set the solution
+
+print(a.get_solution())
