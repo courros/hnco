364b209166e463896725aa1c0876a1fc77ce71db
Arnaud Berny <hnco@courros.fr>
2023-10-26 11:08:30 +0200

    Add optimization

--- bindings/examples/multi-function-complete.py
+++ bindings/examples/multi-function-complete.py
@@ -1,11 +1,15 @@
 import hnco
 import hnco.function as single
 import hnco.multiobjective.function as multi
+import hnco.multiobjective.algorithm as algo
+import hnco.random
+
+hnco.random.Generator.set_seed()
 
 class MyFunction(multi.Function):
 
     def __init__(self, n):
-        super().__init__()
+        multi.Function.__init__(self) # Mandatory
         self.f0 = single.OneMax(n)
         self.f1 = single.LeadingOnes(n)
 
@@ -20,3 +24,12 @@ class MyFunction(multi.Function):
         return 2
 
 f = MyFunction(10)
+
+a = algo.Nsga2(f.get_bv_size(), f.get_output_size(), 100) # population_size = 100
+
+a.set_num_iterations(200)
+a.minimize([f])
+
+solutions = a.get_solutions()
+for bv in solutions.bvs:
+    multi.bv_describe(bv, f)
